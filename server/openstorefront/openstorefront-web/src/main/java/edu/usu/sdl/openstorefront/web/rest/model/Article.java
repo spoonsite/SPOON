71621f3056d96966c2ecf1e66df26e23d35ae81f
dshurtleff <dshurtleff@10.1.100.2>
2014-08-25 23:58:40 -0600

    Tomcat 7 adjustments

---
+++ server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/model/Article.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 Space Dynamics Laboratory - Utah State University Research Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.usu.sdl.openstorefront.web.rest.model;
+
+/**
+ *
+ * @author dshurtleff
+ */
+public class Article
+{
+	private String attributeCode;
+	private String attributeType;
+	private String html;
+
+	public Article()
+	{
+	}
+
+	public String getAttributeCode()
+	{
+		return attributeCode;
+	}
+
+	public void setAttributeCode(String attributeCode)
+	{
+		this.attributeCode = attributeCode;
+	}
+
+	public String getAttributeType()
+	{
+		return attributeType;
+	}
+
+	public void setAttributeType(String attributeType)
+	{
+		this.attributeType = attributeType;
+	}
+
+	public String getHtml()
+	{
+		return html;
+	}
+
+	public void setHtml(String html)
+	{
+		this.html = html;
+	}
+	
+	
+}
