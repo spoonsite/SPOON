8d3b686ce2769591fa4b27cc9aafeaa5609110bf
blaine-o <blaine.esplin@sdl.usu.edu>
2014-08-28 16:56:51 -0600

    Added button clicks at end of script for tags and full page

--- client/openstorefront/test/spec/protractor/searchDetails_spec.js
+++ client/openstorefront/test/spec/protractor/searchDetails_spec.js
@@ -33,11 +33,11 @@ describe('searchDetails_Click on the results', function() {
 
   it('click page tabs to hide parts of page  click tabs again', function() {
     element(by.id('showPageLeft')).click();
-    browser.driver.sleep(1000);
+    browser.driver.sleep(500);
     element(by.id('showPageRight')).click();
-    browser.driver.sleep(1000);
+    browser.driver.sleep(500);
     element(by.id('showPageRight')).click();
-    browser.driver.sleep(1000);
+    browser.driver.sleep(500);
     // Assume if buttons are they they were clicked on (if not visible still there)
     expect(true).toBe(true);
 
@@ -49,5 +49,13 @@ describe('searchDetails_Click on the results', function() {
     expect(element.all(by.binding('results-content-description')));
   });
 
+ it('click on view edit tags and full page', function() {
+    // Click on "View/Edit Tags"
+    element(by.id('data-collapse-tags')).click();
+    // Can't get id (class) of wach button (2 different states)
+    element(by.id('permenantLink')).click();
+    // Check for popped up window, then close
+    expect(true).toBe(true);
+ });
 
 });
