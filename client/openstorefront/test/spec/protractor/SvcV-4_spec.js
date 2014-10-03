146c3512399ab380169186e25288226a6df58290
blaine-O <blaineatwork@yahoo.com>
2014-10-03 14:00:57 -0400

    Updating automation scripts for OpenAM and search changes.  No access to admin tools, removed that script from running

--- client/openstorefront/test/spec/protractor/SvcV-4_spec.js
+++ client/openstorefront/test/spec/protractor/SvcV-4_spec.js
@@ -8,23 +8,35 @@ openAM = true;
 //theSite = 'http://store-prod.usu.di2e.net:8080/openstorefront/index.html';
 //openAM = true;
 
+describe('SvcV-4  Log in to the site if needed', function() {
+    // Log in to the OpenAM page if needed
+    it('Log in to the OpenAM site as required', function() {
+        if (openAM) {
+            // For non-Angular page turn OFF synchronization
+            browser.ignoreSynchronization = true;
+            browser.get(theSite, 3500);
+            browser.driver.sleep(1000);
+            browser.driver.findElement(by.id('IDToken1')).sendKeys('amadmin'); // amadmin
+            browser.driver.findElement(by.id('IDToken2')).sendKeys('password', protractor.Key.ENTER);
+            browser.driver.sleep(1000);
 
-// Log in to the OpenAM page if needed
-if(openAM){
-// For non-Angular page turn OFF synchronization
-browser.ignoreSynchronization = true;
-browser.get(theSite, 3500);
-browser.driver.sleep(3000);
-browser.driver.findElement(by.id('IDToken1')).sendKeys('amadmin');
-browser.driver.findElement(by.id('IDToken2')).sendKeys('password', protractor.Key.ENTER);
-browser.driver.sleep(7000);
-}
+            // refresh past too many redirects error
+            browser.ignoreSyncronization = false;
+            browser.refresh();
+            browser.ignoreSyncronization = false;
+            browser.driver.sleep(3000);
+        }
+    expect(true).toBe(true);
+    });
+
+
+});
 
 describe('SvcV-4_button from the home page', function() {
     it('Expand the buttons in the categories', function () {
         // Navigate to the site
         browser.ignoreSyncronization = false;
-        browser.get(theSite, 3500);
+        browser.get(theSite, 4000);
 
         // Click the SvcV-4 button
         element.all(by.css('.btn.btn-primary.pull-right')).get(1).click();
