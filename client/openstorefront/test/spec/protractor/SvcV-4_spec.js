7944693ea151f02a5387eb17e5ecf17582fa94ac
blaine-O <blaineatwork@yahoo.com>
2014-10-16 18:36:29 -0400

    Broke out the login piece.  Still getting errors on an out of range for the drop-down box on Filters.

--- client/openstorefront/test/spec/protractor/SvcV-4_spec.js
+++ client/openstorefront/test/spec/protractor/SvcV-4_spec.js
@@ -1,34 +1,14 @@
-// GLOBAL Variable for the tests
-//theSite = 'http://di2e.github.io/openstorefront/index.html';
-//openAM = false;
-
-theSite = 'https://storefront1.di2e.net/openstorefront/index.html';
-openAM = true;
-
-
-//theSite = 'http://store-prod.usu.di2e.net:8080/openstorefront/index.html';
-//openAM = true;
-
-// Leave OUTSIDE of describe, it, expect as it is NOT an Angular website page.
-if (openAM) {
-    // For non-Angular page turn OFF synchronization
-    browser.ignoreSynchronization = true;
-    browser.get(theSite, 3500);
-    browser.driver.sleep(1000);
-    browser.driver.findElement(by.id('IDToken1')).sendKeys('amadmin'); // (amadmin) Set to valid account
-    browser.driver.findElement(by.id('IDToken2')).sendKeys('password', protractor.Key.ENTER);
-    browser.driver.sleep(1000);
-}
+/**
+ * Created by blaine.esplin on 10/16/2014.
+ */
 
 describe('SvcV-4_button from the home page', function() {
     it('Expand the buttons in the categories', function () {
         // Navigate to the site
         browser.ignoreSyncronization = false;
         browser.get(theSite, 4000);
-
         // Click the SvcV-4 button
         element.all(by.css('.btn.btn-primary.pull-right')).get(1).click();
-
         browser.driver.sleep(8000);
 
         // Expand
@@ -38,7 +18,6 @@ describe('SvcV-4_button from the home page', function() {
             element.all(by.css('.diagram-toggle-btn')).get(i).click();
             browser.driver.sleep(1);
         }
-
         // Brittle
         expect(element.all(by.css('.btn')).count()).toEqual(7);
     });
