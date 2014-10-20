a685c6329683ac5d44457b0b4a3faf38dd0af408
blaine-O <blaineatwork@yahoo.com>
2014-10-20 15:38:36 -0400

    Change for testing non-openAM MANUAL log-in.  Can't get search filters working at the moment, commented out.

--- client/openstorefront/test/spec/protractor/Login_OpenAM_spec.js
+++ client/openstorefront/test/spec/protractor/Login_OpenAM_spec.js
@@ -1,13 +1,18 @@
 // GLOBAL Variable for the tests
 //theSite = 'http://di2e.github.io/openstorefront/index.html';
-//openAM = false;
-
-theSite = 'https://storefront1.di2e.net/openstorefront/index.html';
-openAM = true;
-
+//openAM = false; other = false;
 
 //theSite = 'http://store-prod.usu.di2e.net:8080/openstorefront/index.html';
-//openAM = true;
+//openAM = true; other = false;
+
+// CURRENT PRODUCTION
+//theSite = 'https://storefront1.di2e.net/openstorefront/index.html';
+//openAM = true; other = false;
+
+// TESTING SITE
+theSite = 'http://store-accept.usu.di2e.net/openstorefront/index.html';
+openAM = false; other = true;
+
 
 // Leave OUTSIDE of describe, it, expect as it is NOT an Angular website page.
 if (openAM) {
@@ -15,9 +20,35 @@ if (openAM) {
     browser.ignoreSynchronization = true;
     browser.get(theSite, 3500);
     browser.driver.sleep(1000);
-    browser.driver.findElement(by.id('IDToken1')).sendKeys('amadmin'); // (amadmin) Set to valid account
-    browser.driver.findElement(by.id('IDToken2')).sendKeys('password', protractor.Key.ENTER);
+    browser.driver.findElement(by.id('IDToken1')).sendKeys('admin'); // (amadmin) Set to valid account
+    browser.driver.findElement(by.id('IDToken2')).sendKeys('secret', protractor.Key.ENTER);
     browser.driver.sleep(1000);
 }
 
-browser.ignoreSynchronization = false;
+// Other Non-OpenAM site, but still need to login!
+if (other) {
+    console.log('**********  Please manually log in, you have 30 seconds.  **********');
+    console.log(theSite);
+    browser.driver.sleep(33000);
+
+    //browser.get('');
+    //browser.ignoreSynchronization = true;
+    //browser.manage().timeouts().pageLoadTimeout(20000);
+    //browser.get(theSite);
+    //browser.waitForUrl('http://store-accept.usu.di2e.net/openstorefront/login.jsp?gotoPage=http://store-accept.usu.di2e.net:8080/openstorefront/');
+
+  /*  abc.wait(function() {
+        return abc.isElementPresent(by.id('username'));
+    }, 20000);
+
+    browser.wait(function() {
+        console.log('waiting...waiting...waiting...waiting...waiting...waiting...waiting...waiting...');
+        browser.driver.findElement(by.id('username')).isDisplayed();
+    });
+  */
+
+    //browser.driver.findElement(by.id('username')).sendKeys('admin'); // (amadmin) Set to valid account
+    //browser.driver.findElement(by.id('password')).sendKeys('secret', protractor.Key.ENTER);
+    //console.log('*******************************************************');
+}
+
