77b45487c21b05f503d18d69fa1f9fe64d302d7f
blaine-O <blaineatwork@yahoo.com>
2014-10-31 12:06:29 -0400

    Finished Search by Tags script

--- client/openstorefront/test/spec/protractor/search_ByTags_spec.js
+++ client/openstorefront/test/spec/protractor/search_ByTags_spec.js
@@ -25,28 +25,33 @@ describe('Search by TAGS', function() {
     }, 44000);
 
 
-    it ('Search by the added tag iAddedThis show result and tag', function() {
+    it ('Search by the added tag iAddedThis and show 1 result', function() {
         // Clear the Search
         element(by.id('globalSearch')).clear();
         element(by.id('globalSearch')).sendKeys(protractor.Key.ENTER);
         expect(element.all(by.repeater('item in data')).count()).toEqual(58);
 
         element.all(by.css('.input.ng-pristine.ng-valid')).get(0).clear();
-        element.all(by.css('.input.ng-pristine.ng-valid')).get(0).sendKeys('iAddedThis', protractor.Key.ENTER);
+        element.all(by.css('.input.ng-pristine.ng-valid')).get(0).sendKeys('iAddedThis', protractor.Key.DOWN, protractor.Key.ENTER);
         browser.driver.sleep(4000);
 
         // Verify one (iSpatial) result
-        //expect(element.all(by.repeater('item in data')).count()).toEqual(1);
+        expect(element.all(by.repeater('item in data')).count()).toEqual(1);
 
         // Click on the result
         element.all(by.css('.results-content-title-content')).get(0).click();
+    });
 
+
+    it ('Delete the newly added tag iAddedThis', function () {
         // Click tags button to show text input field
         element.all(by.css('.fa-tags')).get(0).click();
         browser.driver.sleep(1000);
-    });
 
-    // Delete the tags
+        element.all(by.css('.remove-button.ng-scope')).get(0).click();
+        browser.driver.sleep(3500);
 
+        expect(element.all(by.css('.remove-button.ng-scope')).count()).toEqual(0);
+    })
 
 });
