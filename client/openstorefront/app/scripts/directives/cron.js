f2824be436d8c59afd4fc52b2eca667cfcf83415
Jonathan Law <jedwondle@gmail.com>
2014-11-04 16:10:55 -0700

    Made a directive out of the cron UI.

--- client/openstorefront/app/scripts/directives/cron.js
+++ client/openstorefront/app/scripts/directives/cron.js
@@ -18,25 +18,77 @@
 
 app.directive('cron', ['$timeout', function ($timeout) {
   return {
-    template: '<div></div>',
+    templateUrl: 'views/admin/configuration/cron.html',
     restrict: 'E',
     scope: {
       ngModel: '=',
       callback: '&'
     },
     link: function postLink(scope, element, attrs) {
-      $(function () {
-        var cron = $(element).cronGen();
-        cron[0].addEventListener("cronChange", function(data){
-          console.log('cronChange', data);
-          scope.ngModel = data.detail.cron;
-          scope.$apply(function() {
-            $timeout(function() {
-              scope.callback();
-            })
-          });
-        }, false);
-      });
+
+      scope.generate = function () {
+        var activeTab = $(element).find('li.active').attr('heading');
+        var results = "";
+        switch (activeTab) {
+          case "Minutes":
+          results = "0 0/" + $(element).find("#MinutesInput").val() + " * 1/1 * ? *";
+          break;
+          case "Hourly":
+          switch ($(element).find("input:radio[name=HourlyRadio]:checked").val()) {
+            case "1":
+            results = "0 0 0/" + $(element).find("#HoursInput").val() + " 1/1 * ? *";
+            break;
+            case "2":
+            results = "0 " + Number($(element).find("#AtMinutes").val()) + " " + Number($(element).find("#AtHours").val()) + " 1/1 * ? *";
+            break;
+          }
+          break;
+          case "Daily":
+          switch ($(element).find("input:radio[name=DailyRadio]:checked").val()) {
+            case "1":
+            results = "0 " + Number($(element).find("#DailyMinutes").val()) + " " + Number($(element).find("#DailyHours").val()) + " 1/" + $(element).find("#DaysInput").val() + " * ? *";
+            break;
+            case "2":
+            results = "0 " + Number($(element).find("#DailyMinutes").val()) + " " + Number($(element).find("#DailyHours").val()) + " ? * MON-FRI *";
+            break;
+          }
+          break;
+          case "Weekly":
+          var selectedDays = "";
+          $(element).find("#Weekly input:checkbox:checked").each(function () { selectedDays += $(this).val() + ","; });
+          if (selectedDays.length > 0)
+            selectedDays = selectedDays.substr(0, selectedDays.length - 1);
+          results = "0 " + Number($(element).find("#WeeklyMinutes").val()) + " " + Number($(element).find("#WeeklyHours").val()) + " ? * " + selectedDays + " *";
+          break;
+          case "Monthly":
+          switch ($(element).find("input:radio[name=MonthlyRadio]:checked").val()) {
+            case "1":
+            results = "0 " + Number($(element).find("#MonthlyMinutes").val()) + " " + Number($(element).find("#MonthlyHours").val()) + " " + $(element).find("#DayOfMOnthInput").val() + " 1/" + $(element).find("#MonthInput").val() + " ? *";
+            break;
+            case "2":
+            results = "0 " + Number($(element).find("#MonthlyMinutes").val()) + " " + Number($(element).find("#MonthlyHours").val()) + " ? 1/" + Number($(element).find("#EveryMonthInput").val()) + " " + $(element).find("#DayInWeekOrder").val() + "#" + $(element).find("#WeekDay").val() + " *";
+            break;
+          }
+          break;
+          case "Yearly":
+          switch ($(element).find("input:radio[name=YearlyRadio]:checked").val()) {
+            case "1":
+            results = "0 " + Number($(element).find("#YearlyMinutes").val()) + " " + Number($(element).find("#YearlyHours").val()) + " " + $(element).find("#YearInput").val() + " " + $(element).find("#MonthsOfYear").val() + " ? *";
+            break;
+            case "2":
+            results = "0 " + Number($(element).find("#YearlyMinutes").val()) + " " + Number($(element).find("#YearlyHours").val()) + " ? " + $(element).find("#MonthsOfYear2").val() + " " + $(element).find("#DayWeekForYear").val() + "#" + $(element).find("#DayOrderInYear").val() + " *";
+            break;
+          }
+          break;
+        }
+
+        console.log('results', results);
+        
+        scope.ngModel = results;
+        $timeout(function(){
+          scope.callback();
+        });
+      };
     }
   };
 }]);
