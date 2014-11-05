a79ced6338f55ba033c243a02d98f7dd514076cf
blaine-o <blaine.esplin@sdl.usu.edu>
2014-11-05 08:03:08 -0700

    Added copyright to automation scripts

--- client/openstorefront/test/spec/protractor/adminTools_spec.js
+++ client/openstorefront/test/spec/protractor/adminTools_spec.js
@@ -1,3 +1,18 @@
+/* Copyright 2014 Space Dynamics Laboratory - Utah State University Research Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /* 03 Oct 2014:  Admin tools not available on storefront1.di2e.net.  Devin says it is not expected for IOC?
                   Would have to request privileges from the di2e help desk.
    28 Oct 2014:  Put this back in as Admin Tools are available on store-accept
