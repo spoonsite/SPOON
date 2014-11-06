2bc425ec2a47c2ec705a876736d5610c8f1202ef
blaine-O <blaineatwork@yahoo.com>
2014-11-06 12:55:42 -0500

    Very minor changes; testing first commit to v1.1. branch.

--- client/openstorefront/test/spec/protractor/user_tools_spec.js
+++ client/openstorefront/test/spec/protractor/user_tools_spec.js
@@ -111,10 +111,12 @@ describe('user-tools_User logged in drop-down', function() {
         browser.refresh();
         browser.driver.sleep(4000);
         element.all(by.css('.nav.navbar-nav.navbar-right')).get(0).click();
+
         //element.all(by.css('.dropdown-toggle.ng-binding')).get(0).click();
         element.all(by.css('.dropdown-menu a')).get(3).click();
         browser.driver.sleep(1800);
         expect(element.all(by.css('.btn')).count()).toEqual(7);
+
         element.all(by.css('.close')).get(0).click();
         browser.driver.sleep(5000);
     }, 25000);
