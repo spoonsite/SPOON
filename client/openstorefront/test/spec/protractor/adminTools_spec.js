a5b3b4968dff65bd236930091443a3917767b696
blaine-O <blaineatwork@yahoo.com>
2014-11-13 18:26:12 -0500

    Making sure I have v1.2 branch checked out properly and that I can committ changes.

--- client/openstorefront/test/spec/protractor/adminTools_spec.js
+++ client/openstorefront/test/spec/protractor/adminTools_spec.js
@@ -32,7 +32,7 @@ describe('adminTools', function() {
 
 
     it('TODO-  Change a few values under Global Configuration then change back', function() {
-        /*
+
         // Hourly Tab
         browser.driver.sleep(2000);
         element.all(by.css('.ng-isolate-scope')).get(0).click();
@@ -44,7 +44,7 @@ describe('adminTools', function() {
         element(by.id('AtMinutes')).sendKeys(protractor.Key.ENTER);
 
         element(by.css('.btn.btn-default')).click();
-        */
+
 
     });
 
