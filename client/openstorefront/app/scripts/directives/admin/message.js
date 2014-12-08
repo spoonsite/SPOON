3ad75a2abb55cee543672af26668042c3fc50377
Jonathan Law <jedwondle@gmail.com>
2014-12-08 16:55:36 -0700

    Further work complete on the admin message directive.

--- client/openstorefront/app/scripts/directives/admin/message.js
+++ client/openstorefront/app/scripts/directives/admin/message.js
@@ -20,27 +20,24 @@
 //     <img src='img/content/car.png'/>
 // </message>
 
-app.directive('message', ['$modal', function ($modal) {
+app.directive('message', ['$uiModal', 'business', function ($uiModal, Business) {
   return {
     transclude: true,
     restrict: 'EA',
     template: '<div></div>',
-    scope: {
-      useCtrl: '@',
-      email: '@'
-    },
+    scope: {},
     link: function(scope, element, attrs) {
       scope.$on('$OPENADMINMESSAGE', function() {
-        console.log('We got a click on the button');
-        
         scope.open('lg');
+
       })
       scope.items = ['item1', 'item2', 'item3'];
 
       scope.open = function (size) {
-
-        var modalInstance = $modal.open({
-          templateUrl: 'myModalContent.html',
+        console.log('size', size);
+        
+        var modalInstance = $uiModal.open({
+          templateUrl: 'views/admin/message/adminMessageContent.html',
           controller: 'adminMessageCtrl',
           size: size,
           resolve: {
@@ -53,14 +50,77 @@ app.directive('message', ['$modal', function ($modal) {
         modalInstance.result.then(function (selectedItem) {
           scope.selected = selectedItem;
         }, function () {
-          $log.info('Modal dismissed at: ' + new Date());
+          console.log('Modal dismissed at: ' + new Date());
         });
       };
     }
   };
 }]);
 
-app.controller('adminMessageCtrl', function ($scope, $modalInstance, items) {
+app.directive('contactList', ['$uiModal', 'business', '$q', function ($uiModal, Business, $q) {
+  return {
+    restrict: 'EA',
+    template: '<h5 style="display:inline-block">To:&nbsp;</h5><span style="width: calc(100% - 28px); display: inline-block; float:right"><select class="form-control" style="display:inline-block;" ng-change="checkForToContacts()" ng-model="toField"><option ng-repeat="(key,val) in toOptions">{{val.title}}<button style="float:right;" ng-click="checkForToContacts()" class="btn btn-default btn-small" ng-if="val.refresh"><i class="fa fa-refresh"></i><button></option></select></span>',
+    scope: {},
+    link: function(scope, element, attrs) {
+      scope.getContactList = function() {
+        var deferred = $q.defer();
+        Business.userservice.getAllUserProfiles().then(function(result){
+          deferred.resolve(result);
+          console.log('result', result);
+        }, function(){
+          deferred.reject(false);
+          console.log('There was an error getting the user profiles');
+        }) 
+        return deferred.promise;
+      }
+      scope.getContactList();
+      scope.toOptions = [
+        //
+        {
+          'title': 'All',
+          'value': 'all'
+        }, {
+          'title': 'Specific User Group',
+          'value': 'type',
+          'refresh': true
+        }, {
+          'title': 'Select Users',
+          'value': 'users',
+          'refresh': true
+        }
+      //
+      ]
+      scope.toField = scope.toOptions[0];
+      scope.checkForToContacts = function() {
+        if (scope.toField && (scope.toField.value === 'users' || scope.toField.value === 'type')) {
+          scope.getContactList().then(function(result) {
+            var modalInstance = $uiModal.open({
+              templateUrl: 'views/admin/message/contact.html',
+              controller: 'contactCtrl',
+              size: 'sm',
+              resolve: {
+                type: function () {
+                  return scope.toField.value;
+                }
+              }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+              console.log('selected', selectedItem);
+            }, function () {
+              console.log('Modal dismissed at: ' + new Date());
+            });
+          }, function(){
+            console.log('we clicked on the contact list, but the list failed');
+          });
+        }
+      }
+    }
+  };
+}]);
+
+app.controller('adminMessageCtrl',['$scope', '$uiModalInstance', 'items', 'business', function ($scope, $uiModalInstance, items, Business) {
 
   $scope.items = items;
   $scope.selected = {
@@ -68,10 +128,34 @@ app.controller('adminMessageCtrl', function ($scope, $modalInstance, items) {
   };
 
   $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
+    $uiModalInstance.close($scope.selected.item);
+    console.log('we sent an email', $scope.emails);
   };
 
   $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
+    $uiModalInstance.dismiss('cancel');
   };
-});
+}]);
+
+app.controller('contactCtrl',['$scope', '$uiModalInstance', 'type', 'business', function ($scope, $uiModalInstance, type, Business) {
+  $scope.userTypes = {};
+  $scope.userTypes.types = [];
+  $scope.type = type;
+  if ($scope.type === 'type') {
+    Business.lookupservice.getUserTypeCodes().then(function(result){
+      $scope.userTypes.types = result? result : [];
+      console.log('usertypes', result);
+    }, function(){
+      $scope.userTypes.types = [];
+      console.log('There was an error getting the user profiles');
+    }) 
+  }
+
+  $scope.ok = function (result) {
+    $uiModalInstance.close(result);
+  };
+
+  $scope.cancel = function () {
+    $uiModalInstance.dismiss('cancel');
+  };
+}]);
