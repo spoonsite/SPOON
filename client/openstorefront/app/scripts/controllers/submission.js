b4564b4890b1259dd2283f433dfccde35c434355
unknown <jlaw@C00823.usurf.usu.edu>
2015-04-14 16:41:48 -0600

    More work on the submission form.
    Fixed a few css and html encoding errors.

--- client/openstorefront/app/scripts/controllers/submission.js
+++ client/openstorefront/app/scripts/controllers/submission.js
@@ -18,29 +18,89 @@
 
 /*global isEmpty*/
 
-app.controller('SubmissionCtrl', ['$scope', 'localCache', 'business', '$filter', '$timeout', '$location', '$rootScope', '$q', '$route', '$anchorScroll', 'FileUploader',  function ($scope,  localCache, Business, $filter, $timeout, $location, $rootScope, $q, $route, $anchorScroll, FileUploader) { /*jshint unused: false*/
+app.controller('SubmissionCtrl', ['$scope', 'localCache', 'business', '$filter', '$timeout', '$location', '$rootScope', '$q', '$route', '$anchorScroll', 'FileUploader', '$templateCache', '$uiModal',
+  function ($scope,  localCache, Business, $filter, $timeout, $location, $rootScope, $q, $route, $anchorScroll, FileUploader, $templateCache, $uiModal) { /*jshint unused: false*/
 
-  $scope.test = 'This is a test';
+    $scope.test = 'This is a test';
 
-  $scope.name;
-  $scope.email;
-  $scope.organization;
-  $scope.current = 'top';
+    $scope.name;
+    $scope.email;
+    $scope.organization;
+    $scope.current = 'top';
 
-  $scope.vitals = {};
-  $scope.vitals.name;
-  $scope.vitals.description;
-  $scope.vitals.organization;
-  $scope.vitals.requiredAttributes = [];
 
-  $scope.details = {};
+    $scope.component = {};
+    $scope.component.attributes = [];
+    
+    $scope.component.metadata = [];
+    $scope.metadataLength = 0;
+    $scope.component.metadata[$scope.metadataLength] = {};
+    
+    $scope.component.tags = [];
+    $scope.tagsLength = 0;
+    $scope.component.tags[$scope.tagsLength] = {};
 
-  $scope.getStarted = function(){
-    return $scope.name && $scope.email && $scope.organization;
+    $scope.details = {};
+
+    $scope.getStarted = function(){
+      return $scope.name && $scope.email && $scope.organization;
+    }
+
+    $scope.checkAttributes = function(){
+    // console.log('Compact list', _.compact($scope.component.attributes));
+    // we need to compact the attributes list because it may have unused indexes.
+    var list = angular.copy(_.compact($scope.component.attributes));
+    var requiredAttributes = _.filter(list, {requiredFlg: true});
+    if (requiredAttributes.length !== $scope.requiredAttributes.length) {
+      return false;
+    }
+
+    // This is how we'll weed out the attributes we need for the submission
+    /*
+    var realAttributes = _.compact($scope.component.attributes);
+    var attributes = [];
+    _.each(realAttributes, function(attr){
+      if (attr.constructor === Array){
+        _.each(attr, function(item){
+          attributes.push(item);
+        })
+      } else {
+        attributes.push(attr);
+      }
+    })
+    console.log('attributes', attributes);
+    */
+    return true;
   }
 
-  $scope.vitalsCheck = function(){
-    return $scope.getStarted() && $scope.vitals && $scope.vitals.name && $scope.vitals.description && $scope.vitals.organization && $scope.type;
+  $scope.removeMetadata = function(index){
+    $scope.component.metadata.splice(index, 1);
+    $scope.metadataLength--;
+    $scope.component.metadata[$scope.metadataLength] = {};
+  }
+  $scope.addMetadata = function(){
+    if ($scope.component.metadata[$scope.metadataLength].value && $scope.component.metadata[$scope.metadataLength].label) {
+      $scope.metadataLength++;
+      $scope.component.metadata[$scope.metadataLength] = {};
+      $('#metadataLabel').focus();
+    }
+  }
+
+  $scope.removeTag = function(index){
+    $scope.component.tags.splice(index, 1);
+    $scope.tagsLength--;
+    $scope.component.tags[$scope.tagsLength] = {};
+  }
+  $scope.addTag = function(){
+    if ( $scope.component.tags[$scope.tagsLength].text ) {
+      $scope.tagsLength++;
+      $scope.component.tags[$scope.tagsLength] = {};
+      $('#tagLabel').focus();
+    }
+  }
+
+  $scope.vitalsCheck = function(log){
+    return $scope.getStarted() && $scope.component && $scope.component.name && $scope.component.description && $scope.component.organization && $scope.checkAttributes();
   }
 
   $scope.loadLookup = function(lookup, entity, loader){
@@ -54,12 +114,38 @@ app.controller('SubmissionCtrl', ['$scope', 'localCache', 'business', '$filter',
     });      
   };
 
+  $scope.openInfo = function(attribute){
+    var modalInstance = $uiModal.open({
+      template: $templateCache.get('submission/attributesinfo.tpl.html'),
+      controller: 'AttrsInfoCtrl',
+      size: 'sm',
+      resolve: {
+        size: function() {
+          return 'sm';
+        },
+        attribute: function() {
+          return attribute;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (result) {
+    }, function () {
+    });
+  }
+
   $scope.getAttributes = function (override) { 
     Business.getFilters(override, false).then(function (result) {
       $scope.allAttributes = result ? angular.copy(result) : [];
       $scope.requiredAttributes = _.filter($scope.allAttributes, {requiredFlg: true});
+      console.log('required', $scope.requiredAttributes);
+      _.remove($scope.requiredAttributes, function(attribute) {
+        return attribute.attributeType === 'DI2ELEVEL' || attribute.attributeType === 'DI2EINTENT' || attribute.attributeType === 'DI2ESTATE';
+      });
+      $scope.attributes = _.filter($scope.allAttributes, {requiredFlg: false});
     });
   }; 
+  $scope.getAttributes();
   
   $scope.getCodesForType = function(type){
     var foundType = _.find($scope.allAttributes, {attributeType: type});
@@ -175,4 +261,80 @@ app.controller('SubmissionCtrl', ['$scope', 'localCache', 'business', '$filter',
     })
   })
 
+}])
+.filter('makeattribute', function() {
+  return function(input, attribute) {
+    _.each(input, function(code){
+      if (code) {
+        code.requiredFlg = attribute.requiredFlg || false;
+        code.attributeType = attribute.attributeType;
+      }
+    })
+    return input;
+  };
+})
+.controller('AttrsInfoCtrl', ['$scope', '$uiModalInstance', 'size', 'attribute', 'notificationsFactory', '$timeout', function ($scope, $uiModalInstance, size, attribute, Factory, $timeout) {
+  $scope.attribute = angular.copy(attribute);
+  $scope.title = attribute.description;
+
+  $scope.getDescription = function(code) {
+    return code.description? '<p>'+code.description+'</p>': '<p style="font-style: italic;">There is no detailed description for this code.</p>';
+  }
+
+  $scope.ok = function (validity) {
+    $uiModalInstance.close('success');
+  }
+
+  $scope.cancel = function () {
+    $uiModalInstance.dismiss('cancel');
+  };
+}])
+.controller('myCtrl', function($scope) {
+  $scope.options = [{
+    'label': 'test 1',
+    'code': 'CODE1'
+  }, {
+    'label': 'test 2',
+    'code': 'CODE2'
+  }]
+})
+.directive('multiselect', ['$templateCache', '$timeout', function($templateCache, $timeout) {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      selected: '=',
+      options: '@',
+      list: '=',
+      onChange: '&'
+    },
+    template: $templateCache.get('multiselect/select.tmp.html'),
+    link: function(scope, elem, attrs) {
+      scope.addToSelection = function(selection){
+        console.log('selection', selection);
+        
+        scope.selected = scope.selected && scope.selected.constructor === Array? scope.selected: [];
+        _.contains(scope.selected, selection) || !selection? '':scope.selected.push(selection);
+        scope.onChange(true);
+      }
+      scope.removeItem = function(item){
+        console.log('item', item);
+        
+        var index = _.find(scope.selected, {label: item.label});
+        console.log('index', index);
+        if (index) {
+          index = _.indexOf(scope.selected, index);
+          console.log('index', index);
+          scope.selected.splice(index, 1);
+        }
+        if (scope.selected.length === 0){
+          var elements = elem.find('select')[0].options;
+
+          for(var i = 0; i < elements.length; i++){
+            elements[i].selected = false;
+          }
+        }
+      }
+    }
+  };
 }]);
