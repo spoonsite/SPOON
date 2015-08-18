e76ffaa1293d4a2edc482c7745e6c1e689196c87
blaine-o <blaine.esplin@sdl.usu.edu>
2015-08-18 12:24:04 -0600

    First unit test addition

--- client/openstorefront/test/spec/controllers/admin.js
+++ client/openstorefront/test/spec/controllers/admin.js
@@ -15,9 +15,24 @@
    });
  }));
 
+// besplin, 18 Aug 2015
  it('should have the correct initializations', function () {
-   expect(scope.toolTitle).toEqual('Admin Tools');
    expect(scope.collection).toEqual(null);
+   expect(scope.collectionSelection).toEqual(null);
+   expect(scope.incLoc).toEqual('');
+   expect(scope.saveContent).toEqual('');
+   expect(scope.editedTopic).toEqual('Types');
+   expect(scope.toolTitle).toEqual('Admin Tools');
+   expect(scope.menuPanel.data.open).toEqual(true);
+   expect(scope.menuPanel.system.open).toEqual(true);
+   expect(scope.oneAtATime).toEqual(false);
+   expect(scope.myTree).toEqual(array);
+   expect(scope.systemTree).toEqual(array);
+   expect(scope.data).toEqual(array);
+   expect(scope.systemTools).toEqual(array);
+   expect(scope.menuPanel).toEqual(array);
+   expect(scope.menuPanel.data).toEqual(array);
+   expect(scope.menuPanel.system).toEqual(array);
 
  });
 });
