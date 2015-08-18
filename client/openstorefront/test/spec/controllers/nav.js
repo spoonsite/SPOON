7fe0bd8ba154d31c26b936ca389628664a9520c8
blaine-o <blaine.esplin@sdl.usu.edu>
2015-08-18 17:02:23 -0600

    New unit tests

--- client/openstorefront/test/spec/controllers/nav.js
+++ client/openstorefront/test/spec/controllers/nav.js
@@ -1,22 +1,25 @@
-// 'use strict';
+ 'use strict';
 
-// describe('Controller: NavCtrl', function () {
+ describe('Controller: NavCtrl', function () {
 
-//   // load the controller's module
-//   beforeEach(module('openstorefrontApp'));
+// load the controller's module
+   beforeEach(module('openstorefrontApp'));
 
-//   var NavCtrl,
-//     scope;
+   var NavCtrl,
+     scope;
 
-//   // Initialize the controller and a mock scope
-//   beforeEach(inject(function ($controller, $rootScope) {
-//     scope = $rootScope.$new();
-//     NavCtrl = $controller('NavCtrl', {
-//       $scope: scope
-//     });
-//   }));
+// Initialize the controller and a mock scope
+   beforeEach(inject(function ($controller, $rootScope) {
+     scope = $rootScope.$new();
+     NavCtrl = $controller('NavCtrl', {
+       $scope: scope
+     });
+   }));
 
-//   it('should attach a list of awesomeThings to the scope', function () {
-//     expect(scope.awesomeThings.length).toBe(3);
-//   });
-// });
+   it('should have the correct initializations', function () {
+     expect(scope._scopename).toEqual('nav');
+     expect(scope.navLocation).toEqual('views/nav/nav.html');
+     expect(scope.beforeLogin).toEqual(null);
+     expect(scope.typeahead).toEqual(null);
+   });
+ });
