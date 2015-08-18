e2d6bdd10cd59239f1478f6c78350569e31370ec
blaine-o <blaine.esplin@sdl.usu.edu>
2015-08-18 12:13:36 -0600

    Commit locally

--- client/openstorefront/test/spec/controllers/admin.js
+++ client/openstorefront/test/spec/controllers/admin.js
@@ -1,22 +1,23 @@
-// 'use strict';
+ 'use strict';
 
-// describe('Controller: AdminCtrl', function () {
+ describe('Controller: AdminCtrl', function () {
 
-//   // load the controller's module
-//   beforeEach(module('openstorefrontApp'));
+ // load the controller's module
+ beforeEach(module('openstorefrontApp'));
 
-//   var AdminCtrl,
-//     scope;
+ var AdminCtrl, scope;
 
-//   // Initialize the controller and a mock scope
-//   beforeEach(inject(function ($controller, $rootScope) {
-//     scope = $rootScope.$new();
-//     AdminCtrl = $controller('AdminCtrl', {
-//       $scope: scope
-//     });
-//   }));
+ // Initialize the controller and a mock scope
+ beforeEach(inject(function ($controller, $rootScope) {
+   scope = $rootScope.$new();
+   AdminCtrl = $controller('AdminCtrl', {
+     $scope: scope
+   });
+ }));
 
-//   it('should attach a list of awesomeThings to the scope', function () {
-//     expect(scope.awesomeThings.length).toBe(3);
-//   });
-// });
+ it('should have the correct initializations', function () {
+   expect(scope.toolTitle).toEqual('Admin Tools');
+   expect(scope.collection).toEqual(null);
+
+ });
+});
