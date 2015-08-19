53618a63a67c8aa67a576dd972bed420cad28a0e
blaine-o <blaine.esplin@sdl.usu.edu>
2015-08-19 09:46:11 -0600

    Fixes.  Got the date compare working, also, by truncating the milliseconds and rounding to the nearest second.

--- client/openstorefront/test/spec/controllers/userprofile.js
+++ client/openstorefront/test/spec/controllers/userprofile.js
@@ -5,25 +5,27 @@ describe('Controller: UserProfileCtrl', function () {
   // // load the controller's module
   beforeEach(module('openstorefrontApp'));
 
-  var MainCtrl,
+  var UserProfileCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    UserProfileCtrl = $controller('UserProfileCtrl', {
       $scope: scope
     });
   }));
 
   it('should have the correct initializations', function () {
-    expect(scope._scopename).toEqual('main');  //NOTE this is set to 'userprofile' when scoped!
-    expect(scope.pageTitle).toEqual('DI2E');  //NOTE this is set to 'DI2E Clearinghouse' when scoped!
-    // NOTE:  This does not 'stick' from where it is scoped in userprofile.js in the app
-      //expect(scope.defaultTitle).toEqual('Browse Categories');
-    // NOTE:  This does not 'stick' from where it is scoped in userprofile.js in the app
-      //expect(scope.untilDate).toEqual(new Date());
-    // NOTE:  This does not 'stick' from where it is scoped in userprofile.js in the app
-      //expect(scope.review).toEqual(null);
+    expect(scope._scopename).toEqual('userprofile');  //NOTE this is set to 'userprofile' when scoped!
+    expect(scope.pageTitle).toEqual('DI2E Clearinghouse');  //NOTE this is set to 'DI2E Clearinghouse' when scoped!
+    expect(scope.defaultTitle).toEqual('Browse Categories');
+
+    var coeff = 1000;  // round the milliseconds
+    var thenRounded = new Date(Math.round(scope.untilDate / coeff) * coeff);
+    var nowRounded = new Date(Math.round(new Date() / coeff) * coeff);
+    expect(thenRounded).toEqual(nowRounded);
+
+    expect(scope.review).toEqual(null);
  });
 });
