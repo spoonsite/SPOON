a960d1650553f8c7d02267405b817d53cd03bbc1
tyler-travis <battlemanner@gmail.com>
2015-08-25 15:23:31 -0600

    Added a few unit tests to the directive files

--- client/openstorefront/test/spec/directives/broadcastresults.js
+++ client/openstorefront/test/spec/directives/broadcastresults.js
@@ -1,20 +1,22 @@
-// 'use strict';
+'use strict';
 
-// describe('Directive: broadcastResults', function () {
+describe('Directive: broadcastResults', function () {
 
-//   // load the directive's module
-//   beforeEach(module('openstorefrontApp'));
+  // load the directive's module
+  beforeEach(module('openstorefrontApp'));
 
-//   var element,
-//     scope;
+  var element,
+    scope;
 
-//   beforeEach(inject(function ($rootScope) {
-//     scope = $rootScope.$new();
-//   }));
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
 
-//   it('should make hidden element visible', inject(function ($compile) {
-//     element = angular.element('<broadcast-results></broadcast-results>');
-//     element = $compile(element)(scope);
-//     expect(element.text()).toBe('this is the broadcastResults directive');
-//   }));
-// });
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<broadcast-results></broadcast-results>');
+    element = $compile(element)(scope);
+    expect(element.html()).toContain('<div></div>');
+    
+    
+  }));
+});
