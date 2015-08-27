a7b0af387e8c58cc7f71d0a43bd41f0ea9ac0183
blaine-o <blaine.esplin@sdl.usu.edu>
2015-08-27 15:31:11 -0600

    More cleanup and more adding new checks

--- client/openstorefront/test/spec/controllers/submission.js
+++ client/openstorefront/test/spec/controllers/submission.js
@@ -19,8 +19,20 @@ describe('Controller: submission', function () {
   it('should have the correct initializations', function () {
     expect(scope.test).toEqual('This is a test');
     expect(scope.badgeFound).toEqual(false);
-    expect(scrope.lastMediaFile).toEqual('');
-    expect(scope.hideMultiSlect).toEqual('true');
+    expect(scope.lastMediaFile).toEqual('');
+    expect(scope.hideMultiSelect).toEqual(true);
 
+    expect(scope.editor.editorContent).toEqual('');
+    expect(scope.editor.editorContentWatch);
+
+    expect(scope.submitter.firstName).toBe(undefined);
+    expect(scope.current).toEqual('top');
+    expect(scope.optIn).toEqual(true);
+    expect(scope.notDif).toEqual(true);
+
+    expect(scope.showMediaUpload).toEqual('falseValue');
+    expect(scope.showResourceUpload).toEqual('trueValue');
   });
+
+  alert('Controller: submission; should have the correct initializations = PASS (12 expects)');
 });
