996cf1f02c9350a2adf6b827522dfaf26de5663c
dshurt <dshurt@gmail.com>
2015-10-19 10:32:55 -0600

    STORE-324  Branding Support adding colors

--- server/openstorefront/openstorefront-core/common/src/main/java/edu/usu/sdl/openstorefront/common/util/OpenStorefrontConstant.java
+++ server/openstorefront/openstorefront-core/common/src/main/java/edu/usu/sdl/openstorefront/common/util/OpenStorefrontConstant.java
@@ -64,6 +64,7 @@ public class OpenStorefrontConstant
 	public static final int FIELD_SIZE_16K = 16384;
 	public static final int FIELD_SIZE_32K = 32768;
 	public static final int FIELD_SIZE_64K = 65536;
+	public static final int FIELD_SIZE_1MB = 1048576;
 
 	public static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
