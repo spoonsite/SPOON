48d3a62a6fa66f656f3f41f7317c21e38054786b
Jacob Bottelberghe <jacob.bottelberghe@sdl.usu.edu>
2016-04-25 13:36:21 -0600

    Save component type list on POST
    The list needs to be saved separately upon the POSTing of a new/modified
    attribute type.

--- server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/resource/AttributeResource.java
+++ server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/resource/AttributeResource.java
@@ -389,6 +389,14 @@ public class AttributeResource
 		} else if (attributeTypeSave.getComponentTypeRestrictions() == null) {
 			attributeType.setRequiredRestrictions(attributeTypeSave.getComponentTypeRestrictions());
 		}
+
+		if (attributeTypeSave.getAssociatedComponentTypes() != null
+				&& attributeTypeSave.getAssociatedComponentTypes().isEmpty() == false) {
+			attributeType.setAssociatedComponentTypes(attributeTypeSave.getAssociatedComponentTypes());
+		} else if (attributeTypeSave.getAssociatedComponentTypes() == null) {
+			attributeType.setAssociatedComponentTypes(attributeTypeSave.getAssociatedComponentTypes());
+		}
+
 		return handleAttributePostPutType(attributeType, true);
 	}
 
