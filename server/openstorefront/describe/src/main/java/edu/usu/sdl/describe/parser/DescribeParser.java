efa9b358f67fd29610a39bcf50008ffb9dcfc285
dshurt <devin.shurtleff@sdl.usu.edu>
2016-09-29 14:04:19 -0600

    Updating module version 2.2

--- server/openstorefront/describe/src/main/java/edu/usu/sdl/describe/parser/DescribeParser.java
+++ server/openstorefront/describe/src/main/java/edu/usu/sdl/describe/parser/DescribeParser.java
@@ -24,48 +24,49 @@ import org.simpleframework.xml.strategy.Strategy;
  */
 public class DescribeParser
 {
-	private static final Logger log = Logger.getLogger(DescribeParser.class.getName());
-	
+
+	private static final Logger LOG = Logger.getLogger(DescribeParser.class.getName());
+
 	public static TrustedDataCollection parse(File xmlfile) throws Exception
 	{
 		return DescribeParser.parse(new FileInputStream(xmlfile));
 	}
-	
+
 	public static TrustedDataCollection parse(InputStream in) throws Exception
 	{
 		Strategy strategy = new AnnotationStrategy();
-                Serializer serializer = new Persister(strategy);
+		Serializer serializer = new Persister(strategy);
 		TrustedDataCollection data = null;
-		try{
-			data = serializer.read(TrustedDataCollection.class, in);			
+		try {
+			data = serializer.read(TrustedDataCollection.class, in);
 		} finally {
 			if (in != null) {
-				try{
-					in.close();		
-				} catch(IOException ioe) {
-					log.log(Level.WARNING, "Unable to close describe file.");					
+				try {
+					in.close();
+				} catch (IOException ioe) {
+					LOG.log(Level.WARNING, "Unable to close describe file.");
 				}
 			}
 		}
 		return data;
 	}
-	
+
 	public static void write(OutputStream out, TrustedDataCollection dataCollection) throws Exception
 	{
 		Strategy strategy = new AnnotationStrategy();
-                Serializer serializer = new Persister(strategy);
-				
-		try{		
+		Serializer serializer = new Persister(strategy);
+
+		try {
 			serializer.write(dataCollection, out);
 		} finally {
 			if (out != null) {
-				try{
-					out.close();		
-				} catch(IOException ioe) {
-					log.log(Level.WARNING, "Unable to close describe file.");					
+				try {
+					out.close();
+				} catch (IOException ioe) {
+					LOG.log(Level.WARNING, "Unable to close describe file.");
 				}
-			}			
+			}
 		}
-	}	
-	
+	}
+
 }
