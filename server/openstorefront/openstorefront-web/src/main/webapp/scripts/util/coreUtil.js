219602faa0a0fc40dd4663ff3217afb77ded5579
Jacob Bottelberghe <jacob.bottelberghe@sdl.usu.edu>
2016-12-08 13:06:05 -0700

    Replace TinyMCE image plugin with osfmediainserter plugin
    STORE-1220

--- server/openstorefront/openstorefront-web/src/main/webapp/scripts/util/coreUtil.js
+++ server/openstorefront/openstorefront-web/src/main/webapp/scripts/util/coreUtil.js
@@ -381,10 +381,10 @@ var CoreUtil = {
 			"advlist autolink lists link image charmap print preview hr anchor pagebreak",
 			"searchreplace wordcount visualblocks visualchars code fullscreen",
 			"insertdatetime media nonbreaking save table contextmenu directionality",
-			"emoticons template paste textcolor placeholder"
+			"emoticons template paste textcolor placeholder osfmediainserter"
 			],
 
-			toolbar1: "formatselect | bold italic underline forecolor backcolor | bullist numlist | outdent indent | alignleft aligncenter alignright | image media charmap | link savedsearchlink table | fullscreen",
+			toolbar1: "formatselect | bold italic underline forecolor backcolor | bullist numlist | outdent indent | alignleft aligncenter alignright | osfmediainserter media charmap | link savedsearchlink table | fullscreen",
 
 			content_css : "contents.css",
 
@@ -414,10 +414,10 @@ var CoreUtil = {
 			"advlist autolink lists link image charmap print preview hr anchor pagebreak",
 			"searchreplace wordcount visualblocks visualchars code fullscreen",
 			"insertdatetime media nonbreaking save table contextmenu directionality",
-			"emoticons template paste textcolor placeholder savedsearchlink"
+			"emoticons template paste textcolor placeholder savedsearchlink osfmediainserter"
 			],
 
-			toolbar1: "formatselect | bold italic underline forecolor backcolor | bullist numlist | outdent indent | alignleft aligncenter alignright | image media charmap | link savedsearchlink table | fullscreen",
+			toolbar1: "formatselect | bold italic underline forecolor backcolor | bullist numlist | outdent indent | alignleft aligncenter alignright | osfmediainserter media charmap | link savedsearchlink table | fullscreen",
 
 			content_css : "contents.css",
 
