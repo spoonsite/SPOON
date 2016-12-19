83098d5187adb00724f8ce469465fc74b7364f3d
dshurt <devin.shurtleff@sdl.usu.edu>
2016-12-19 16:34:02 -0700

    STORE-1304 Update User

--- server/openstorefront/openstorefront-core/model/src/main/java/edu/usu/sdl/openstorefront/core/entity/StandardEntity.java
+++ server/openstorefront/openstorefront-core/model/src/main/java/edu/usu/sdl/openstorefront/core/entity/StandardEntity.java
@@ -100,11 +100,12 @@ public abstract class StandardEntity<T>
 			this.setActiveStatus(entity.getActiveStatus());
 		}
 
+		this.populateBaseUpdateFields();
+		
 		if (StringUtils.isNotBlank(entity.getUpdateUser())) {
 			setUpdateUser(entity.getUpdateUser());
 		}
 
-		this.populateBaseUpdateFields();
 	}
 
 	public void populateBaseUpdateFields()
@@ -112,10 +113,9 @@ public abstract class StandardEntity<T>
 		if (StringUtils.isBlank(getActiveStatus())) {
 			setActiveStatus(ACTIVE_STATUS);
 		}
-		setUpdateDts(TimeUtil.currentDate());
-		if (StringUtils.isBlank(getUpdateUser())) {
-			setUpdateUser(SecurityUtil.getCurrentUserName());
-		}
+		setUpdateDts(TimeUtil.currentDate());		
+		setUpdateUser(SecurityUtil.getCurrentUserName());
+		
 		if (getAdminModified() == null) {
 			setAdminModified(SecurityUtil.isAdminUser());
 		}
