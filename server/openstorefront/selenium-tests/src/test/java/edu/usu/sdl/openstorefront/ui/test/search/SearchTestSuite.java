d20cbe9eae79fe6d69c251b08429eb197578f0f8
Cameron Cummings <cscummings5@yahoo.com>
2017-10-02 12:18:49 -0600

    Revert "Revert "STORE 2178 - Removed files that are no longer needed and created class for Adding tag to entry""
    This reverts commit c50a6bb979d9c2b0b1edf731e5b863e97f3d46e0.

--- server/openstorefront/selenium-tests/src/test/java/edu/usu/sdl/openstorefront/ui/test/search/SearchTestSuite.java
+++ server/openstorefront/selenium-tests/src/test/java/edu/usu/sdl/openstorefront/ui/test/search/SearchTestSuite.java
@@ -25,9 +25,10 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	SimpleSearchIT.class,
-	SearchResultsIT.class,
-	AdvanceSearchIT.class
+	BasicSearchIT.class,
+	PrintSearchResultsEntryIT.class,
+	ViewSearchResultEntryIT.class,
+	AddTagSearchResultEntryIT.class
 })
 public class SearchTestSuite
 {
