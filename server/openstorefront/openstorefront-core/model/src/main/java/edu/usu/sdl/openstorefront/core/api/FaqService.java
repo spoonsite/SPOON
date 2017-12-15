e3ed82beee32f9e00d03fdc0a73d538df508432a
cyearsley <caleb.yearsley@sdl.usu.edu>
2017-12-14 18:24:43 -0700

    STORE-2211 Feature: add temp/testing rest endpoints.

---
+++ server/openstorefront/openstorefront-core/model/src/main/java/edu/usu/sdl/openstorefront/core/api/FaqService.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2017 Space Dynamics Laboratory - Utah State University Research Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.usu.sdl.openstorefront.core.api;
+
+import edu.usu.sdl.openstorefront.core.entity.Faq;
+import java.util.List;
+
+/**
+ *
+ * @author cyearsley
+ */
+public interface FaqService
+		extends AsyncService
+{
+	/**
+	 * Gets all active FAQs
+	 *
+	 * @return List<Faq>
+	 */
+	public List<Faq> getFaqs();
+	
+	/**
+	 * Get a single Faq
+	 *
+	 * @param faqId
+	 * @return Faq
+	 */
+	public Faq getFaq(String faqId);
+	
+	/**
+	 * Set a Faq question
+	 *
+	 * @param faqId
+	 * @param question
+	 * @return
+	 */
+	public void setFaqQuestion(String faqId, String question);
+	
+	/**
+	 * Set a Faq answer
+	 *
+	 * @param evaluationId
+	 * @param publicInformationOnly
+	 * @return
+	 */
+	public void setFaqAnswer(String faqId, String answer);
+	
+	/**
+	 * Updates a Faq
+	 *
+	 * @param fiqId
+	 * @param faq
+	 * @return
+	 */
+	public void updateFaq(String fiqId, Faq faq);
+}
---
+++ server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/resource/FaqResource.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2017 Space Dynamics Laboratory - Utah State University Research Foundation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * See NOTICE.txt for more information.
+ */
+package edu.usu.sdl.openstorefront.web.rest.resource;
+
+import edu.usu.sdl.openstorefront.core.annotation.APIDescription;
+import edu.usu.sdl.openstorefront.core.annotation.DataType;
+import edu.usu.sdl.openstorefront.core.entity.Faq;
+import edu.usu.sdl.openstorefront.core.entity.SecurityPermission;
+import edu.usu.sdl.openstorefront.core.view.ComponentLookupModel;
+import edu.usu.sdl.openstorefront.core.view.RequiredForComponent;
+import edu.usu.sdl.openstorefront.core.view.UserProfileView;
+import edu.usu.sdl.openstorefront.doc.annotation.RequiredParam;
+import edu.usu.sdl.openstorefront.doc.security.RequireSecurity;
+import java.util.Arrays;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author cyearsley
+ */
+@Path("v1/resource/faq")
+@APIDescription("An FAQ contains data in regards to frequently asked quesitons and answers.")
+public class FaqResource
+		extends BaseResource
+{
+	@GET
+	@APIDescription("Gets a list of all FAQs")
+	@Produces({MediaType.APPLICATION_JSON})
+	@DataType(Faq.class)
+	public Response faqLookup()
+	{
+		Faq faqExample1 = new Faq();
+		Faq faqExample2 = new Faq();
+		
+		faqExample1.setAnswer("This is the answer to the FAQ (Example 1)");
+		faqExample1.setQuestion("This is the question to the FAQ (Example 1)");
+		faqExample1.setActiveStatus("A");
+		faqExample1.setCategory("CAT type for example 1");
+		
+		faqExample2.setQuestion("This is the question to the FAQ (Example 2)");
+		faqExample2.setAnswer("This is the answer to the FAQ (Example 2)");
+		faqExample2.setActiveStatus("A");
+		faqExample2.setCategory("CAT type for example 2");
+		
+		GenericEntity<List<Faq>> entity = new GenericEntity<List<Faq>>(Arrays.asList(faqExample1,faqExample2))
+			{
+			};
+		return sendSingleEntityResponse(entity);
+	}
+	
+	@POST
+	@APIDescription("Creates an FAQ")
+	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({MediaType.APPLICATION_JSON})
+	@DataType(Faq.class)
+	public Response createFaq(Faq faq)
+	{
+		
+		return Response.accepted().build();
+	}
+	
+	@PUT
+	@APIDescription("Updates an FAQ")
+	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({MediaType.APPLICATION_JSON})
+	@DataType(Faq.class)
+	@Path("/{id}")
+	public Response updateFaq(
+			@PathParam("id") String faqId,
+			Faq faq
+	)
+	{
+		
+		return Response.ok().build();
+	}
+	
+	@DELETE
+	@APIDescription("Deletes an FAQ")
+	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({MediaType.APPLICATION_JSON})
+	@DataType(Faq.class)
+	@Path("/{id}")
+	public Response deleteFaq(@PathParam("id") String faqId)
+	{
+		
+		return Response.accepted().build();
+	}
+}
