055e254fd7437a754d2f3eea73296aa2a08268c1
Cameron Cummings <cscummings5@yahoo.com>
2018-01-18 15:57:23 -0700

    Added provider package and two providers - attribute and component

--- server/openstorefront/selenium-tests/src/main/java/edu/usu/sdl/openstorefront/selenium/test/storeLoginAcceptDiffBrowsers.java
+++ server/openstorefront/selenium-tests/src/main/java/edu/usu/sdl/openstorefront/selenium/test/storeLoginAcceptDiffBrowsers.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.usu.sdl.openstorefront.selenium.tests;
+package edu.usu.sdl.openstorefront.selenium.test;
 
 /**
  *
