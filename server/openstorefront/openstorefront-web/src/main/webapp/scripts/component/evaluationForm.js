ec323107c2eb694687c6b55d160b9a37a73e644d
cyearsley <caleb.yearsley@sdl.usu.edu>
2018-01-18 09:51:12 -0700

    STORE-2390 Improvement: instead of explicitly setting the content panel's form to 'read only', add a listener to the content panel for when something is added (then, in the content panel, set the form to 'read only'.)

--- server/openstorefront/openstorefront-web/src/main/webapp/scripts/component/evaluationForm.js
+++ server/openstorefront/openstorefront-web/src/main/webapp/scripts/component/evaluationForm.js
@@ -27,6 +27,7 @@ Ext.define('OSF.component.RootEvaluationPanel', {
 		});
 
 		self.contentPanel = Ext.create('Ext.panel.Panel', {
+
 			region: 'center',			
 			layout: 'fit',
 			itemId: 'contentPanel',
@@ -83,6 +84,23 @@ Ext.define('OSF.component.RootEvaluationPanel', {
 					]
 				}
 			]			
+		});
+
+		self.contentPanel.on('add', function () {
+			if (self.readOnly) {
+				Ext.Array.forEach(self.contentPanel.query('button'), function (field, index) {
+					field.setVisible(false);
+					field.setDisabled(true);
+				});
+				Ext.Array.forEach(self.contentPanel.query('grid'), function (grid, index) {
+					grid.setStyle('opacity', '0.6');
+					grid.events = {};
+				});
+				Ext.Array.forEach(self.contentPanel.query('field'), function (field) {
+					field.setReadOnly(true);
+					field.setStyle('opacity', '0.6');
+				});
+			}
 		});
 
 		self.commentPanel = Ext.create('Ext.panel.Panel', {
@@ -517,24 +535,6 @@ Ext.define('OSF.component.RootEvaluationPanel', {
 					commentPanel: self.commentPanel,
 					user: self.user,
 					mainForm: self
-				}, function () {
-
-					// if readOnly, disable/hide the appropriate fields for the content form
-					if (self.readOnly) {
-						
-						Ext.Array.forEach(self.contentPanel.query('button'), function (field, index) {
-							field.setVisible(false);
-							field.setDisabled(true);
-						});
-						Ext.Array.forEach(self.contentPanel.query('grid'), function (grid, index) {
-							grid.setStyle('opacity', '0.6');
-							grid.events = {};
-						});
-						Ext.Array.forEach(self.contentPanel.query('field'), function (field) {
-							field.setReadOnly(true);
-							field.setStyle('opacity', '0.6');
-						});
-					}
 				});
 			}
 		});
