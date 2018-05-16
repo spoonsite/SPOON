9783d75369f9c0fe134d711b4d8b89e1dceee72a
Ryan Frazier <pianomanfrazier@gmail.com>
2018-05-16 10:34:08 -0600

    STORE-3058 add page stubs for mobile

---
+++ client/mobile/src/components/FAQPage.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section class="faq-page">
+    <h1>faq-page Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+export default {
+  name: 'faq-page',
+  props: [],
+  mounted () {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.faq-page {
+
+}
+</style>
---
+++ client/mobile/src/components/UserProfilePage.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section class="user-profile-page">
+    <h1>user-profile-page Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+export default {
+  name: 'user-profile-page',
+  props: [],
+  mounted () {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .user-profile-page {
+
+  }
+</style>
