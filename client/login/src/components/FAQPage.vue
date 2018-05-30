d8e745d5147d22446acacc87b2863f713de5dd56
Aaron Fine <fine.aaron@gmail.com>
2018-05-30 17:09:57 -0600

    STORE-3126 Mobile FAQ
    Added initial layout to FAQ page

--- client/login/src/components/FAQPage.vue
+++ client/login/src/components/FAQPage.vue
@@ -1,14 +1,19 @@
 <template>
 <div class="wrapper">
   <h2>Frequently Asked Questions</h2>
-  <v-expansion-panel>
-    <v-expansion-panel-content v-for="(item,i) in 5" :key="i">
-    <div slot="header">Item</div>
-    <v-card>
-      <v-card-text>Lorem ipsum</v-card-text>
-    </v-card>
-    </v-expansion-panel-content>
-  </v-expansion-panel>
+
+<v-layout mt-3>
+    <v-flex xs12>
+      <v-expansion-panel focusable>
+        <v-expansion-panel-content v-for="(item,i) in 5" :key="i">
+        <div slot="header" class="title">Item</div>
+        <v-card>
+          <v-card-text class="grey lighten-3">Lorem ipsum</v-card-text>
+        </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-flex>
+  </v-layout>
 </div>
 </template>
 
