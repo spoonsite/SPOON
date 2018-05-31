4612fa70c98f533b667727481ee2e4ae91e4bafb
jacstrong <ja.ha.strong@gmail.com>
2018-05-31 16:13:43 -0600

    STORE-3127 still cleaning up

--- client/login/src/router/index.js
+++ client/login/src/router/index.js
@@ -4,10 +4,7 @@ import LoginPage from '@/components/LoginPage'
 import ForgotUserPage from '@/components/ForgotUserPage'
 import ForgotPasswordPage from '@/components/ForgotPasswordPage'
 import RegistrationPage from '@/components/RegistrationPage'
-<<<<<<< HEAD
-=======
 import ContactUsPage from '@/components/ContactUsPage'
->>>>>>> STORE-3127_mobile_contact_us
 import FAQPage from '@/components/FAQPage'
 
 Vue.use(Router)
@@ -35,14 +32,11 @@ export default new Router({
       component: RegistrationPage
     },
     {
-<<<<<<< HEAD
-=======
       path: '/contact-us',
       name: 'contactUs',
       component: ContactUsPage
     },
     {
->>>>>>> STORE-3127_mobile_contact_us
       path: '/faq',
       name: 'faq',
       component: FAQPage
