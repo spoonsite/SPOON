6327b82aacac80880b3e167da9425ad9348efa46
Ryan Frazier <pianomanfrazier@gmail.com>
2018-06-04 16:19:05 -0600

    STORE-3063 connect forgot username and forgot password to backend

--- client/login/src/components/ForgotUserPage.vue
+++ client/login/src/components/ForgotUserPage.vue
@@ -1,33 +1,66 @@
 <template>
-  <div>
-    <v-layout align-center justify-center>
-      <forgot-username-comp/>
-    </v-layout>
-  </div>
+<div>
+  <v-layout style="max-width: 35em; margin-right: auto; margin-left: auto;" >
+    <v-flex xs12>
+      <v-card class="elevation-5 mt-3 mx-3">
+        <v-toolbar color="primary" dark dense>
+          <v-toolbar-title>Forgot Username</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form v-model="valid" @submit.prevent="submitEmail()">
+            <v-text-field
+              prepend-icon="email"
+              name="email"
+              label="Enter Email"
+              type="text"
+              v-model="email"
+              :rules="emailRules"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn :disabled="!valid" block color="accent" @click="submitEmail()">Send Username</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+
+  <v-dialog v-model="dialog" max-width="300px">
+    <v-card tile>
+      <v-card-text>Check your email for you username(s).</v-card-text>
+      <v-card-actions>
+        <v-btn @click="$router.push('/')"><v-icon>fas fa-return</v-icon> Return to Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</div>
 </template>
 
 <script>
-import ForgotUsernameComp from './SubComps/ForgotUsernameComp';
-
 export default {
-  name: 'ForgotUserPage',
-  components: {
-    ForgotUsernameComp
-  },
-  computed: {
+  name: 'ForgotUsernameComp',
+  data: () => ({
+    dialog: false,
+    valid: false,
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v =>
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+        'E-mail must be valid'
+    ]
+  }),
+  methods: {
+    submitEmail () {
+      this.$http.get(`/openstorefront/api/v1/service/security/forgotusername?emailAddress=${this.email}`)
+        .then(response => {
+          this.dialog = true;
+        })
+        .catch(error => console.log(error));
+    }
   }
 };
 </script>
 
 <style scoped>
-.login-comp {
-  width: 50%;
-  height: 100%;
-  margin: 2em;
-}
-.signup-comp {
-  width: 50%;
-  height: 100%;
-  margin: 2em;
-}
 </style>
