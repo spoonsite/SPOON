5aeb644a4d232c821b6dc9c5e3807743405f05d5
Ryan Frazier <pianomanfrazier@gmail.com>
2018-06-04 17:06:09 -0600

    STORE-3063 bind component data to model in login credentials for registration page

--- client/login/src/components/SubComps/RegistrationCredentialsComp.vue
+++ client/login/src/components/SubComps/RegistrationCredentialsComp.vue
@@ -8,13 +8,34 @@
         <v-card-text>
           <v-form v-model="valid">
             <v-layout row>
-              <v-text-field prepend-icon="person" name="username" label="Username" type="text" :rules="usernameRules"></v-text-field>
+              <v-text-field
+                prepend-icon="person"
+                name="username"
+                label="Username"
+                type="text"
+                :rules="usernameRules"
+                v-model="value.username"
+              ></v-text-field>
             </v-layout>
             <v-layout row>
-            <v-text-field prepend-icon="lock" name="password" label="Password" type="text" :rules="passwordRules"></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+                :rules="password1Rules"
+                v-model="value.password1"
+              ></v-text-field>
             </v-layout>
             <v-layout row>
-            <v-text-field prepend-icon="lock" name="confirmpassword" label="Confirm Password" type="text" :rules="confirmPasswordRules"></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="confirmpassword"
+                label="Confirm Password"
+                type="password"
+                :rules="password2Rules"
+                v-model="value.password2"
+              ></v-text-field>
             </v-layout>
           </v-form>
         </v-card-text>
@@ -28,16 +49,28 @@
 <script>
 export default {
   name: 'RegistrationCredentialsComp',
-  data: () => ({
-    valid: false,
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v =>
-        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-        'E-mail must be valid'
-    ]
-  }),
+  props: ['value'],
+  data: function () {
+    return {
+      valid: false,
+      usernameRules: [
+        v => !!v || 'Username is required',
+        v =>
+          /^\w+$/.test(v) ||
+          'Username must be valid'
+      ],
+      password1Rules: [
+        v => !!v || 'Password is required',
+        v =>
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~`!@#$%^&*()-+=<>:;"',.?])[A-Za-z\d~`!@#$%^&*()-+=<>:;"',.?]{8,}$/.test(v) ||
+          'Password must contain 1 uppercase, 1 number, and 1 special character (i.e. @$!%*#?&)'
+      ],
+      password2Rules: [
+        v => !!v || 'Password verification is required',
+        v => this.value.password1 === this.value.password2 || 'Passwords must match'
+      ]
+    };
+  },
   methods: {
     testAPI: function (event) {
       console.log('Button Clicked Calling API');
