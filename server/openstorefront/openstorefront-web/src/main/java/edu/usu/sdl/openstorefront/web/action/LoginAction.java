3e23c6e471483ba2089b55f3376669ee68376b07
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2018-06-13 11:25:04 -0600

    STORE-3124 add logout and desktop links, route mobile users

--- server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/action/LoginAction.java
+++ server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/action/LoginAction.java
@@ -18,7 +18,9 @@ package edu.usu.sdl.openstorefront.web.action;
 import edu.usu.sdl.openstorefront.common.exception.OpenStorefrontRuntimeException;
 import edu.usu.sdl.openstorefront.common.manager.PropertiesManager;
 import edu.usu.sdl.openstorefront.common.util.NetworkUtil;
+import edu.usu.sdl.openstorefront.common.util.OpenStorefrontConstant;
 import edu.usu.sdl.openstorefront.core.entity.UserProfile;
+import edu.usu.sdl.openstorefront.service.manager.UserAgentManager;
 import edu.usu.sdl.openstorefront.core.view.JsonResponse;
 import edu.usu.sdl.openstorefront.security.HeaderRealm;
 import edu.usu.sdl.openstorefront.security.SecurityUtil;
@@ -34,6 +36,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
+import net.sf.uadetector.ReadableUserAgent;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ErrorResolution;
 import net.sourceforge.stripes.action.ForwardResolution;
@@ -135,7 +138,17 @@ public class LoginAction
 
 		UserContext userContext = SecurityUtil.getUserContext();
 		if (userContext != null) {
-			startPage = userContext.userLandingPage();
+			String userAgent = getContext().getRequest().getHeader(OpenStorefrontConstant.HEADER_USER_AGENT);
+			ReadableUserAgent readableUserAgent = UserAgentManager.parse(userAgent);
+			switch (readableUserAgent.getDeviceCategory().getCategory()) {
+				case SMARTPHONE:
+				case TABLET:
+					startPage = "/mobile/index.html"; // this doesn't work
+					break;
+				default:
+					startPage = userContext.userLandingPage();
+					break;
+			}
 		}
 
 		if (StringUtils.isNotBlank(gotoPage)) {
