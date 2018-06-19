c23d92f4817aff66eda9bab29a2f36240ac27035
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2018-06-19 17:10:11 -0600

    STORE-3070 create lightbox component

--- client/mobile/src/components/EntryDetailPage.vue
+++ client/mobile/src/components/EntryDetailPage.vue
@@ -7,42 +7,10 @@
       </v-card-text>
     </v-card>
 
-    <!-- MEDIA carousel -->
-    <div
+    <Lightbox
       v-if="detail.componentMedia && detail.componentMedia.length !== 0"
-      class="mediaWrapper  grey lighten-2"
-      >
-      <div class="mediaItem">
-        <img
-          v-for="(item, index) in lightboxList"
-          :key="item.link"
-          :src="baseURL+item.link"
-          class="mediaImage elevation-4"
-          @click="lightboxOn(index)"
-        >
-      </div>
-    </div>
-
-    <!-- LIGHTBOX Popup -->
-    <div v-if="lightbox" class="lightbox">
-      <div class="lightboxImageWrapper">
-        <img :src="lightboxCurrentImage" class="lightboxImage elevation-3">
-      </div>
-
-      <div class="lightboxControl">
-        <v-btn v-if="lightboxList.length > 1" flat icon small color="yellow" @click="lightboxPrev()">
-          <v-icon dark>navigate_before</v-icon>
-        </v-btn>
-
-        <v-btn flat icon small color="yellow" style="margin-left: 2em; margin-right: 2em;" @click="lightbox = false;">
-          <v-icon dark>clear</v-icon>
-        </v-btn>
-
-        <v-btn v-if="lightboxList.length > 1" flat icon small color="yellow" @click="lightboxNext()">
-          <v-icon dark>navigate_next</v-icon>
-        </v-btn>
-      </div>
-    </div>
+      :lightboxList="lightboxList"
+    ></Lightbox>
 
     <v-expansion-panel popout>
 
@@ -262,11 +230,13 @@
 <script lang="js">
 import StarRating from 'vue-star-rating';
 import _ from 'lodash';
+import Lightbox from './subcomponents/Lightbox';
 
 export default {
   name: 'entry-detail-page',
   components: {
-    StarRating
+    StarRating,
+    Lightbox
   },
   mounted () {
     if (this.$route.params.id) {
@@ -283,9 +253,6 @@ export default {
       watchSwitch: false,
       hasImage: false,
       lightboxList: [],
-      lightbox: false,
-      lightboxCurrentImage: '',
-      lightboxCurrentIndex: 0,
       attributeTableHeaders: [
         {
           text: 'Attribute Type',
@@ -345,23 +312,6 @@ export default {
           }
         }
       }
-    },
-    lightboxOn (index) {
-      this.lightbox = true;
-      this.lightboxCurrentIndex = index;
-      this.lightboxCurrentImage = this.baseURL + this.lightboxList[index].link;
-      this.lightboxSetImage();
-    },
-    lightboxSetImage () {
-      this.lightboxCurrentImage = this.baseURL + this.lightboxList[this.lightboxCurrentIndex].link;
-    },
-    lightboxNext () {
-      this.lightboxCurrentIndex = this.lightboxList.length % (this.lightboxCurrentIndex + 1);
-      this.lightboxSetImage();
-    },
-    lightboxPrev () {
-      this.lightboxCurrentIndex = this.lightboxList.length > 0 ? this.lightboxCurrentIndex - 1 : this.lightboxList.length;
-      this.lightboxSetImage();
     }
   },
   computed: {
@@ -384,54 +334,6 @@ export default {
   }
   .contactPar {
     margin-bottom: 0.5em;
-  }
-  .lightbox {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 990;
-    background-color: rgba(71, 71, 71, 0.7);
-    pointer-events: all;
-  }
-  .lightboxControl {
-    bottom: 0%;
-    position: fixed;
-    width: 100%;
-    z-index: 991;
-    text-align: center;
-    background-color: rgba(39, 39, 39, 0.7);
-  }
-  .lightboxImage {
-    background: #fff;
-    max-width: 98%;
-    max-height: 90%;
-  }
-  .lightboxImageWrapper {
-    margin-top: 1em;
-    width: 100%;
-    max-height: 90%;
-    position: absolute;
-    top: 45%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-  }
-  .mediaWrapper {
-    overflow-x: scroll;
-    overflow-y: hidden;
-    white-space: nowrap;
-    margin-bottom: 1em;
-  }
-  .mediaItem{
-    float: left;
-    display: block;
-  }
-  .mediaImage {
-    background: #fff;
-    margin: 1em;
-    max-height: 8em;
   }
   .overlay {
     position: fixed;
--- client/mobile/src/components/subcomponents/Lightbox.vue
+++ client/mobile/src/components/subcomponents/Lightbox.vue
@@ -1,25 +1,77 @@
 <template>
 <div>
+    <!-- MEDIA carousel -->
+    <div
+      class="mediaWrapper  grey lighten-2"
+      >
+      <div class="mediaItem">
+        <img
+          v-for="(item, index) in lightboxList"
+          :key="item.link"
+          :src="baseURL+item.link"
+          class="mediaImage elevation-4"
+          @click="lightboxOn(index)"
+        >
+      </div>
+    </div>
 
+    <!-- LIGHTBOX Popup -->
+    <div v-if="lightbox" class="lightbox">
+      <div class="lightboxImageWrapper">
+        <img :src="lightboxCurrentImage" class="lightboxImage elevation-3">
+      </div>
+
+      <div class="lightboxControl">
+        <v-btn v-if="lightboxList.length > 1" flat icon small color="yellow" @click="lightboxPrev()">
+          <v-icon dark>navigate_before</v-icon>
+        </v-btn>
+
+        <v-btn flat icon small color="yellow" style="margin-left: 2em; margin-right: 2em;" @click="lightbox = false;">
+          <v-icon dark>clear</v-icon>
+        </v-btn>
+
+        <v-btn v-if="lightboxList.length > 1" flat icon small color="yellow" @click="lightboxNext()">
+          <v-icon dark>navigate_next</v-icon>
+        </v-btn>
+      </div>
+    </div>
 </div>
 </template>
 
 <script>
-import _ from 'lodash';
-
 export default {
   name: 'Lightbox',
-  props: ['list'],
+  props: ['lightboxList'],
   mounted () {
   },
   data () {
     return {
+      baseURL: '/openstorefront/',
+      lightbox: false,
+      lightboxCurrentImage: '',
+      lightboxCurrentIndex: 0
     };
   },
   methods: {
+    lightboxOn (index) {
+      this.lightbox = true;
+      this.lightboxCurrentIndex = index;
+      this.lightboxCurrentImage = this.baseURL + this.lightboxList[index].link;
+      this.lightboxSetImage();
+    },
+    lightboxSetImage () {
+      this.lightboxCurrentImage = this.baseURL + this.lightboxList[this.lightboxCurrentIndex].link;
+    },
+    lightboxNext () {
+      this.lightboxCurrentIndex = this.lightboxList.length % (this.lightboxCurrentIndex + 1);
+      this.lightboxSetImage();
+    },
+    lightboxPrev () {
+      this.lightboxCurrentIndex = this.lightboxList.length > 0 ? this.lightboxCurrentIndex - 1 : this.lightboxList.length;
+      this.lightboxSetImage();
+    }
   },
   computed: {
-
   },
   watch: {
   }
@@ -27,4 +79,52 @@ export default {
 </script>
 
 <style>
+  .lightbox {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 990;
+    background-color: rgba(71, 71, 71, 0.7);
+    pointer-events: all;
+  }
+  .lightboxControl {
+    bottom: 0%;
+    position: fixed;
+    width: 100%;
+    z-index: 991;
+    text-align: center;
+    background-color: rgba(39, 39, 39, 0.7);
+  }
+  .lightboxImage {
+    background: #fff;
+    max-width: 98%;
+    max-height: 90%;
+  }
+  .lightboxImageWrapper {
+    margin-top: 1em;
+    width: 100%;
+    max-height: 90%;
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+  .mediaWrapper {
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+    margin-bottom: 1em;
+  }
+  .mediaItem{
+    float: left;
+    display: block;
+  }
+  .mediaImage {
+    background: #fff;
+    margin: 1em;
+    max-height: 8em;
+  }
 </style>
