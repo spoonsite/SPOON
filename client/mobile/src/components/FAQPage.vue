a5cba89f0b0a4d221e1ed8e646cf7b087750bf8d
jacstrong <ja.ha.strong@gmail.com>
2018-07-06 13:13:49 -0600

    STORE-3146 copied from login side

--- client/mobile/src/components/FAQPage.vue
+++ client/mobile/src/components/FAQPage.vue
@@ -1,34 +1,47 @@
-<template lang="html">
+<template>
+<v-layout mt-3 mx-3>
+  <v-flex xs12 md4 offset-md4 sm6 offset-sm3>
+    <h2 class="text-xs-center">Frequently Asked Questions</h2>
 
-  <section class="faq-page">
-    <h1>faq-page Component</h1>
-  </section>
-
+    <v-expansion-panel popout>
+      <v-expansion-panel-content v-for="entry in questions" :key="entry.faqSortOrder" ripple>
+      <div slot="header" class="title">{{entry.question}}</div>
+      <v-card>
+        <v-card-text class="grey lighten-5" v-html="entry.answer"></v-card-text>
+      </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-flex>
+</v-layout>
 </template>
 
-<script lang="js">
+<script>
+import axios from 'axios';
+
 export default {
-  name: 'faq-page',
-  props: [],
-  mounted () {
-
-  },
-  data () {
-    return {
-
-    };
-  },
+  name: 'FAQPage',
+  data: () => ({
+    questions: [],
+    errors: []
+  }),
   methods: {
+    getQuestions: function () {
+      let that = this;
 
+      axios
+        .get('/openstorefront/api/v1/resource/faq')
+        .then(response => {
+          var filtered = response.data.filter(item => item.activeStatus === 'A');
+          that.questions = filtered;
+        })
+        .catch(e => this.errors.push(e));
+    }
   },
-  computed: {
-
+  mounted () {
+    this.getQuestions();
   }
 };
 </script>
 
-<style scoped lang="scss">
-.faq-page {
-
-}
+<style>
 </style>
