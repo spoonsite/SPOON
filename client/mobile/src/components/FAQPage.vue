4c8e60ef0964c59217eee7a56b85d541e49feda2
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2018-07-09 16:07:28 -0600

    STORE-3146 remove redundant title from FAQ page

--- client/mobile/src/components/FAQPage.vue
+++ client/mobile/src/components/FAQPage.vue
@@ -1,8 +1,6 @@
 <template>
 <v-layout mt-3 mx-3>
   <v-flex xs12 md4 offset-md4 sm6 offset-sm3>
-    <h2 class="text-xs-center">Frequently Asked Questions</h2>
-
     <v-expansion-panel popout>
       <v-expansion-panel-content v-for="entry in questions" :key="entry.faqSortOrder" ripple>
       <div slot="header" class="title">{{entry.question}}</div>
