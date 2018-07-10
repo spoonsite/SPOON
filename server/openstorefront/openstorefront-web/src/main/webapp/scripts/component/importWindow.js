01909638e270f522831e6304f931b8165373f841
cyearsley <caleb.yearsley@sdl.usu.edu>
2018-07-10 14:04:36 -0600

    STORE-3027 fix: fix import/export permission typo

--- server/openstorefront/openstorefront-web/src/main/webapp/scripts/component/importWindow.js
+++ server/openstorefront/openstorefront-web/src/main/webapp/scripts/component/importWindow.js
@@ -46,7 +46,7 @@ Ext.define('OSF.component.ImportWindow', {
 							text: 'Upload',
 							formBind: true,
 							iconCls: 'fa fa-lg fa-upload icon-button-color-default',
-							requiredPermissions: ['ADMIN_DATA_IMPORT_EXPORT'],
+							requiredPermissions: ['ADMIN-DATA-IMPORT-EXPORT'],
 							handler: function () {
 								var uploadForm = this.up('form');
 								var data = uploadForm.getValues();
