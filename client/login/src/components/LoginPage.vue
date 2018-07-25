6ba1831cbb0b4957f20118d65d1de71dda2b4423
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2018-07-25 15:41:32 -0600

    STORE-3149 add login content block to login page

--- client/login/src/components/LoginPage.vue
+++ client/login/src/components/LoginPage.vue
@@ -2,6 +2,11 @@
 <v-layout mt-3 mx-3>
 <v-flex xs12 md4 offset-md4 sm6 offset-sm3>
     <video controls class="video elevation-5" v-if="$store.state.branding.loginOverviewVideoUrl" :src="$store.state.branding.loginOverviewVideoUrl" :poster="$store.state.branding.loginOverviewVideoPosterUrl"></video>
+    <v-card v-if="$store.state.branding.loginContentBlock" class="elevation-5 my-4">
+      <v-card-text>
+        <div v-html="$store.state.branding.loginContentBlock"></div>
+      </v-card-text>
+    </v-card>
     <login-comp></login-comp>
   </v-flex>
 </v-layout>
