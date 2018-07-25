49559928160fe581a78cebd687fe8caef06412a2
Caleb Yearsley <cyearsley@sdl.usu.edu>
2018-07-25 12:03:19 -0600

    STORE-3211 Fix: couldn't run storefront

--- server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/resource/WorkplanResource.java
+++ server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/resource/WorkplanResource.java
@@ -32,6 +32,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -161,8 +162,8 @@ public class WorkplanResource
 	@Path("/assign/component/{id}")
 	public Response assignWorkplanForComponent(
 			@PathParam("id") String componentId,
-			String username,
-			String roleGroup
+			@QueryParam("username") String username,
+			@QueryParam("roleGroup") String roleGroup
 	)
 	{
 		service.getWorkPlanService().assignWorkPlanForComponent(componentId, username, roleGroup);
