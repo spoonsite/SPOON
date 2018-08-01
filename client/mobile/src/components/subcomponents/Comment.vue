ac24bcfb9cee4fd4b629e5c1bc9a4773fd5f482d
jacstrong <ja.ha.strong@gmail.com>
2018-08-01 13:52:41 -0600

    STORE-3074 implement comment component

---
+++ client/mobile/src/components/subcomponents/Comment.vue
@@ -0,0 +1,127 @@
+<template lang="html">
+
+  <section class="comment-box">
+    <div>
+      <v-container pa-3>
+        <div class="white elevation-2" style="overflow: auto;">
+          <div v-bind:class="isOwner(comment.createUser)">
+
+            <h3>{{ comment.createUser }}
+              <span style="float: right" v-if="$store.state.currentUser.username === comment.createUser">
+                <v-tooltip bottom>
+                  <v-btn small slot="activator" icon @click="openEditCommentDialog()">   <v-icon class="icon">edit</v-icon></v-btn>
+                  <span>Edit the question</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <v-btn small slot="activator" icon @click="deleteCommentDialog = true"><v-icon class="icon">delete</v-icon></v-btn>
+                  <span>Delete the question</span>
+                </v-tooltip>
+              </span>
+            </h3>
+
+          </div>
+          <div class="pa-2">
+            <strong>Comment date: </strong>
+            <span>{{ comment.createDts | formatDate }}</span>
+
+            <div class="pa-2" v-html="comment.comment">
+            </div>
+          </div>
+        </div>
+      </v-container>
+    </div>
+
+      <v-dialog
+        v-model="deleteCommentDialog"
+        max-width="300px"
+        >
+        <v-card>
+          <v-card-text>
+            <p>Are you sure you want to delete this comment?</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="warning" @click="deleteComment()"><v-icon>delete</v-icon> Delete</v-btn>
+            <v-btn @click="deleteCommentDialog = false">Cancel</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+    <v-dialog
+      v-model="editCommentDialog"
+      >
+      <v-card>
+        <v-card-title>
+          <h2 class="w-100">Edit a Comment</h2>
+          <v-alert class="w-100" type="warning" :value="true"><span v-html="$store.state.branding.userInputWarning"></span></v-alert>
+        </v-card-title>
+        <v-card-text>
+          <quill-editor
+          style="background-color: white;"
+          v-model="newComment"
+          ></quill-editor>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="success" @click="editComment(comment.commentId)">Submit</v-btn>
+          <v-btn @click="editCommentDialog = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+  </section>
+
+</template>
+
+<script lang="js">
+
+export default {
+  name: 'Comment',
+  props: ['comment'],
+  mounted () {
+    if (this.$route.params.id) {
+      this.id = this.$route.params.id;
+    }
+
+    this.$http.get('http://localhost:3005/submission')
+      .then(response => {
+        if (response.data) {
+          this.comments = response.data;
+        }
+      });
+  },
+  data () {
+    return {
+      id: '',
+      deleteCommentDialog: false,
+      editCommentDialog: false,
+      newComment: ''
+    };
+  },
+  methods: {
+    deleteComment () {
+      // implement when you have an endpoint.
+    },
+    editComment (commentId) {
+      // implement when you have an endpoint.
+    },
+    isOwner (createUser) {
+      if (this.$store.state.currentUser.username === createUser) {
+        return 'cyan accent-4 pa-2';
+      }
+      return 'grey lighten-2 pa-2';
+    },
+    openEditCommentDialog () {
+      this.newComment = this.comment.comment;
+      this.editCommentDialog = true;
+    }
+  },
+  computed: {
+
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .comments {
+
+  }
+</style>
