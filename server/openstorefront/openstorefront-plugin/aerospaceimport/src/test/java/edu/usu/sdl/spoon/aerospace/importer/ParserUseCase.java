85bbb3deabd0b68f0556313610935b2644094806
dshurt <dshurt@gmail.com>
2018-10-23 11:01:48 -0600

    STORE-3607 Setting up plugin STORE-3606 Working on the comparison report

---
+++ server/openstorefront/openstorefront-plugin/aerospaceimport/src/test/java/edu/usu/sdl/spoon/aerospace/importer/ParserUseCase.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.usu.sdl.spoon.aerospace.importer;
+
+/**
+ *
+ * @author dshurtleff
+ */
+public class ParserUseCase
+{
+
+}
