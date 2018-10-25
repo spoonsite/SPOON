4b3169bb775b03ae998d2301c1257f4ec6b6c7d1
dshurt <dshurt@gmail.com>
2018-10-25 15:15:27 -0600

    STORE-3596 working on parsing

--- server/openstorefront/openstorefront-plugin/aerospaceimport/src/main/java/edu/usu/sdl/spoon/aerospace/importor/model/ProductRevision.java
+++ server/openstorefront/openstorefront-plugin/aerospaceimport/src/main/java/edu/usu/sdl/spoon/aerospace/importor/model/ProductRevision.java
@@ -15,6 +15,8 @@
  */
 package edu.usu.sdl.spoon.aerospace.importor.model;
 
+import org.simpleframework.xml.Element;
+
 /**
  *
  * @author dshurtleff
@@ -22,11 +24,125 @@ package edu.usu.sdl.spoon.aerospace.importor.model;
 public class ProductRevision
 {
 
-	String fromDate;
-	String thruDate;
-	Specs specs;
-	Additional additional;
-	ProductType productType;
-	ProductFamily productFamily;
+	@Element(name = "from_date")
+	private String fromDate;
+
+	@Element(name = "thru_date")
+	private String thruDate;
+
+	@Element
+	private Specs specs;
+
+	@Element
+	private Additional additional;
+
+	@Element(name = "product_type")
+	private ProductType productType;
+
+	@Element(name = "product_family")
+	private ProductFamily productFamily;
+
+	@Element
+	private Provenance provenance;
+
+	@Element(name = "comments")
+	private Comment comment;
+
+	@Element(name = "organizations")
+	private Organizations organizations;
+
+	public ProductRevision()
+	{
+	}
+
+	public String getFromDate()
+	{
+		return fromDate;
+	}
+
+	public void setFromDate(String fromDate)
+	{
+		this.fromDate = fromDate;
+	}
+
+	public String getThruDate()
+	{
+		return thruDate;
+	}
+
+	public void setThruDate(String thruDate)
+	{
+		this.thruDate = thruDate;
+	}
+
+	public Specs getSpecs()
+	{
+		return specs;
+	}
+
+	public void setSpecs(Specs specs)
+	{
+		this.specs = specs;
+	}
+
+	public Additional getAdditional()
+	{
+		return additional;
+	}
+
+	public void setAdditional(Additional additional)
+	{
+		this.additional = additional;
+	}
+
+	public ProductType getProductType()
+	{
+		return productType;
+	}
+
+	public void setProductType(ProductType productType)
+	{
+		this.productType = productType;
+	}
+
+	public ProductFamily getProductFamily()
+	{
+		return productFamily;
+	}
+
+	public void setProductFamily(ProductFamily productFamily)
+	{
+		this.productFamily = productFamily;
+	}
+
+	public Provenance getProvenance()
+	{
+		return provenance;
+	}
+
+	public void setProvenance(Provenance provenance)
+	{
+		this.provenance = provenance;
+	}
+
+	public Comment getComment()
+	{
+		return comment;
+	}
+
+	public void setComment(Comment comment)
+	{
+		this.comment = comment;
+	}
+
+	public Organizations getOrganizations()
+	{
+		return organizations;
+	}
+
+	public void setOrganizations(Organizations organizations)
+	{
+		this.organizations = organizations;
+	}
 
 }
