0632a481e43ac9a5957af4a5302ecac9d2732607
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2018-11-29 14:27:00 -0700

    STORE-3596 stub out activator and parser for aerospace

--- server/openstorefront/openstorefront-plugin/aerospaceimport/src/main/java/edu/usu/sdl/spoon/aerospace/importor/Activator.java
+++ server/openstorefront/openstorefront-plugin/aerospaceimport/src/main/java/edu/usu/sdl/spoon/aerospace/importor/Activator.java
@@ -1,16 +1,43 @@
 package edu.usu.sdl.spoon.aerospace.importor;
 
+import edu.usu.sdl.openstorefront.core.api.Service;
+import edu.usu.sdl.openstorefront.core.api.ServiceProxyFactory;
+import edu.usu.sdl.openstorefront.core.entity.FileFormat;
+import edu.usu.sdl.openstorefront.core.entity.FileType;
+import java.util.logging.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
+    private static final Logger log = Logger.getLogger(Activator.class.getName());
 
-    public void start(BundleContext context) throws Exception {
-        // TODO add activation code here
-    }
+   public void start(BundleContext context) throws Exception {
+        log.info("Starting plugin example");
+       //Register new parser format
+       Service service = ServiceProxyFactory.getServiceProxy();
 
-    public void stop(BundleContext context) throws Exception {
-        // TODO add deactivation code here
-    }
+       if (service.getSystemService().isSystemReady()
+               || service.getSystemService().isLoadingPluginsReady()) {
+           FileFormat aerospaceFormat = new FileFormat();
+           aerospaceFormat.setCode(AerospaceParser.FORMAT_CODE);
+           aerospaceFormat.setFileType(FileType.COMPONENT);
+           aerospaceFormat.setDescription("Aerospace Import (ZIP)");
+           aerospaceFormat.setSupportsDataMap(true);
+           aerospaceFormat.setParserClass(AerospaceParser.class.getName());
+
+           service.getImportService().registerFormat(aerospaceFormat, AerospaceParser.class);
+       }
+   }
+
+   public void stop(BundleContext context) throws Exception {
+        log.info("Stopping plugin example");
+       Service service = ServiceProxyFactory.getServiceProxy();
+
+       if (service.getSystemService().isSystemReady()
+               || service.getSystemService().isLoadingPluginsReady()) {
+           //unregister parsers
+           service.getImportService().unregisterFormat(AerospaceParser.class.getName());
+       }
+   }
 
 }
---
+++ server/openstorefront/openstorefront-plugin/aerospaceimport/src/main/java/edu/usu/sdl/spoon/aerospace/importor/AerospaceParser.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 Space Dynamics Laboratory - Utah State University Research Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.usu.sdl.spoon.aerospace.importor;
+
+import edu.usu.sdl.openstorefront.core.spi.parser.BaseComponentParser;
+import edu.usu.sdl.openstorefront.core.spi.parser.reader.GenericReader;
+import edu.usu.sdl.openstorefront.core.spi.parser.reader.XMLMapReader;
+import java.io.InputStream;
+
+/**
+ *
+ * @author rfrazier
+ */
+public class AerospaceParser
+        extends BaseComponentParser {
+
+    public static final String FORMAT_CODE = "SPOONCMP";
+
+    @Override
+    public String checkFormat(String mimeType, InputStream input) {
+        if (mimeType.contains("xml")) {
+            return "";
+        } else {
+            return "Invalid format. Please upload a XML file.";
+        }
+    }
+
+    @Override
+    protected GenericReader getReader(InputStream in) {
+        return new XMLMapReader(in);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    protected <T> Object parseRecord(T record) {
+        return null;
+    }
+
+}
