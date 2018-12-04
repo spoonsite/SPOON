f4919291993762f0b6315e2916455e2b1365dda7
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2018-12-04 14:36:23 -0700

    STORE-3596 add component mapping csv and map

--- server/openstorefront/openstorefront-plugin/aerospaceimport/src/main/java/edu/usu/sdl/spoon/aerospace/importer/Parser.java
+++ server/openstorefront/openstorefront-plugin/aerospaceimport/src/main/java/edu/usu/sdl/spoon/aerospace/importer/Parser.java
@@ -15,10 +15,22 @@
  */
 package edu.usu.sdl.spoon.aerospace.importer;
 
+import au.com.bytecode.opencsv.CSVReader;
+import edu.usu.sdl.openstorefront.core.entity.ComponentAttribute;
 import edu.usu.sdl.openstorefront.core.model.ComponentAll;
+import edu.usu.sdl.spoon.aerospace.importer.model.Classification;
 import edu.usu.sdl.spoon.aerospace.importer.model.Product;
 import edu.usu.sdl.spoon.aerospace.importer.model.Services;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
@@ -27,9 +39,46 @@ import org.simpleframework.xml.core.Persister;
  * @author rfrazier
  */
 public class Parser {
+    // map aerospace attributes to our attributes
+    //     get this from the admin UI
+    // map product type to component type
+    //     programmer defined mappings
+    
+    private static final Logger log = Logger.getLogger(Activator.class.getName());
+    private Map<String, String> componentTypeMap;
+    
+    Parser() {
+        try {
+            componentTypeMap = getComponentTypeMap();
+        } catch (Exception e) {
+            log.severe("Unable to initialize component type map from CSV: " + e.toString());
+        }
+    }
+
     
     /**
-     * 
+     * reads the component type mapping CSV in the class resources and
+     * returns a map of Aerospace product type to Storefront component type
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public Map<String, String> getComponentTypeMap() throws FileNotFoundException, IOException {
+        Map<String, String> componentTypeMap = new HashMap<>();
+
+        Reader in = new FileReader(this.getClass().getResource("/componentTypeMapping.csv").getFile());
+        CSVReader reader = new CSVReader(in);
+        
+        String[] record;
+        while ((record = reader.readNext()) != null) {
+            componentTypeMap.put(record[0], record[1]);
+        }
+
+        return componentTypeMap;
+    }
+
+    /**
+     *
      * @param in an input XML stream
      * @return a the root of the document i.e. a parsed services object
      */
@@ -45,17 +94,35 @@ public class Parser {
 
         return services;
     }
-    
+
     /**
-     * 
+     *
      * @param product the parsed root of the XML document
      * @return a mapped component with all data mapped to storefront
      */
     public ComponentAll productToComponentAll(Product product) {
         ComponentAll result = new ComponentAll();
-        
+
         // map the data in the product to attributes and data in the componentAll
-        
+        return result;
+    }
+
+    public String getComponentType(Product product) {
+        List<Classification> classifications = product.getProductRevision().getProductFamily().getClassification();
+        String result = "";
+
+        if (classifications.size() > 0) {
+            String name = classifications.get(0).getCategoryName();
+            // map Aerospace classification to storefront componentType
+            result = componentTypeMap.get(name);
+        }
+
+        return result;
+    }
+
+    public List<ComponentAttribute> getComponentAttributes(Product product) {
+        List<ComponentAttribute> result = new ArrayList();
+
         return result;
     }
 }
