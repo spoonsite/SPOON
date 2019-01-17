147ca750134736dac21d8c71e2b499f9ffd2af7c
Brigham Michaelis <brigham.michaelis@sdl.usu.edu>
2019-01-17 16:26:52 -0700

    STORE-3686 This commit will fix the reports permission problems.

--- server/openstorefront/openstorefront-core/model/src/main/java/edu/usu/sdl/openstorefront/core/entity/ReportType.java
+++ server/openstorefront/openstorefront-core/model/src/main/java/edu/usu/sdl/openstorefront/core/entity/ReportType.java
@@ -30,20 +30,20 @@ import java.util.Map;
 public class ReportType
 		extends LookupEntity<ReportType>
 {
-
-	public static final String USAGE = "USAGE";
-	public static final String LINK_VALIDATION = "LINKVALID";
-	public static final String COMPONENT = "COMPONENT";
-	public static final String USER = "USER";
-	public static final String ORGANIZATION = "ORGANIZATION";
-	public static final String COMPONENT_ORGANIZATION = "CMPORG";
-	public static final String SUBMISSION = "SUBMISSION";
-	public static final String CATEGORY_COMPONENT = "CATCOMP";
-	public static final String COMPONENT_DETAIL = "TYPECOMP";
-	public static final String EVALUATION_STATUS = "EVALSTAT";
+	
 	public static final String ACTION_REPORT = "ACTION";
-	public static final String ENTRY_LISTING = "ENTRYLIST";
-	public static final String ENTRY_STATUS = "ENTRYSTATUS";
+	public static final String ENTRIES_BY_CAT_REPORT = "CATCOMP";
+	public static final String ENTRIES_BY_ORG_REPORT = "CMPORG";
+	public static final String ENTRY_DETAIL_REPORT = "TYPECOMP";
+	public static final String ENTRY_LISTING_REPORT = "ENTRYLIST";
+	public static final String ENTRY_REPORT = "COMPONENT";
+	public static final String ENTRY_STATUS_REPORT = "ENTRYSTATUS";
+	public static final String EVAL_STATUS_REPORT = "EVALSTAT";
+	public static final String LINK_VALIDATION_REPORT = "LINKVALID";
+	public static final String SUBMISSIONS_REPORT = "SUBMISSION";
+	public static final String USAGE_REPORT = "USAGE";
+	public static final String USER_ORG_REPORT = "ORGANIZATION";
+	public static final String USER_REPORT = "USER";
 	public static final String WORKPLAN_STATUS = "WORKPLANSTATUS";
 
 	private String requiredPermission;
@@ -58,40 +58,45 @@ public class ReportType
 	protected Map<String, LookupEntity> systemCodeMap()
 	{
 		Map<String, LookupEntity> codeMap = new HashMap<>();
-		codeMap.put(USAGE, newLookup(ReportType.class, USAGE, "Usage", "Reports on overall application usage."));
-		codeMap.put(LINK_VALIDATION, newLookup(ReportType.class, LINK_VALIDATION, "Link Validation", "Reports on potentially broken external url links."));
-		codeMap.put(COMPONENT, newLookup(ReportType.class, COMPONENT, "Entry", "Reports on entry statistic and usage."));
-		codeMap.put(USER, newLookup(ReportType.class, USER, "User", "Reports on users and their usage of the application."));
-		codeMap.put(ORGANIZATION, newLookup(ReportType.class, ORGANIZATION, "User Organization", "Reports on the user organizations in the application and their usage."));
-		codeMap.put(COMPONENT_ORGANIZATION, newLookup(ReportType.class, COMPONENT_ORGANIZATION, "Entries by Organization", "Reports on entries that belong to an organization"));
-		codeMap.put(SUBMISSION, newLookup(ReportType.class, SUBMISSION, "Submissions", "Reports on entry submissions."));
-		codeMap.put(CATEGORY_COMPONENT, newLookup(ReportType.class, CATEGORY_COMPONENT, "Entries by Category", "Reports on entries in a category."));
-		codeMap.put(EVALUATION_STATUS, newLookup(ReportType.class, EVALUATION_STATUS, "Evaluation Status", "Reports on the status of unpublished evaluations."));
+		
 		codeMap.put(ACTION_REPORT, newLookup(ReportType.class, ACTION_REPORT, "Action", "Report on items that need administrative attention."));
-		codeMap.put(COMPONENT_DETAIL, newLookup(ReportType.class, COMPONENT_DETAIL, "Entry Detail", "Exports entry details"));
-		codeMap.put(ENTRY_LISTING, newLookup(ReportType.class, ENTRY_LISTING, "Entry Listing", "List approved entries in a summary"));
-		codeMap.put(ENTRY_STATUS, newLookup(ReportType.class, ENTRY_STATUS, "Entry Status", "Gathers information about entry status for a time period."));
+		codeMap.put(ENTRIES_BY_CAT_REPORT, newLookup(ReportType.class, ENTRIES_BY_CAT_REPORT, "Entries by Category", "Reports on entries in a category."));
+		codeMap.put(ENTRIES_BY_ORG_REPORT, newLookup(ReportType.class, ENTRIES_BY_ORG_REPORT, "Entries by Organization", "Reports on entries that belong to an organization"));	
+		codeMap.put(ENTRY_DETAIL_REPORT, newLookup(ReportType.class, ENTRY_DETAIL_REPORT, "Entry Detail", "Exports entry details"));
+		codeMap.put(ENTRY_LISTING_REPORT, newLookup(ReportType.class, ENTRY_LISTING_REPORT, "Entry Listing", "List approved entries in a summary"));
+		codeMap.put(ENTRY_REPORT, newLookup(ReportType.class, ENTRY_REPORT, "Entry", "Reports on entry statistic and usage."));
+		codeMap.put(ENTRY_STATUS_REPORT, newLookup(ReportType.class, ENTRY_STATUS_REPORT, "Entry Status", "Gathers information about entry status for a time period."));
+		codeMap.put(EVAL_STATUS_REPORT, newLookup(ReportType.class, EVAL_STATUS_REPORT, "Evaluation Status", "Reports on the status of unpublished evaluations."));
+		codeMap.put(LINK_VALIDATION_REPORT, newLookup(ReportType.class, LINK_VALIDATION_REPORT, "Link Validation", "Reports on potentially broken external url links."));
+		codeMap.put(SUBMISSIONS_REPORT, newLookup(ReportType.class, SUBMISSIONS_REPORT, "Submissions", "Reports on entry submissions."));
+		codeMap.put(USAGE_REPORT, newLookup(ReportType.class, USAGE_REPORT, "Usage", "Reports on overall application usage."));
+		codeMap.put(USER_ORG_REPORT, newLookup(ReportType.class, USER_ORG_REPORT, "User Organization", "Reports on the user organizations in the application and their usage."));
+		codeMap.put(USER_REPORT, newLookup(ReportType.class, USER_REPORT, "User", "Reports on users and their usage of the application."));
 		codeMap.put(WORKPLAN_STATUS, newLookup(ReportType.class, WORKPLAN_STATUS, "Workplan Status", "Shows the current status of items in workplans"));
 
 		//update metadata
-		((ReportType) codeMap.get(USAGE)).setRequiredPermission(SecurityPermission.RUN_USAGE_REPORT);
-		((ReportType) codeMap.get(LINK_VALIDATION)).setRequiredPermission(SecurityPermission.RUN_LINK_VALIDATION_REPORT);
-		((ReportType) codeMap.get(USER)).setRequiredPermission(SecurityPermission.RUN_USER_ORG_REPORT);
-		((ReportType) codeMap.get(SUBMISSION)).setRequiredPermission(SecurityPermission.RUN_SUBMISSIONS_REPORT);
-		((ReportType) codeMap.get(ORGANIZATION)).setRequiredPermission(SecurityPermission.RUN_ENTRIES_BY_ORG_REPORT);
-		((ReportType) codeMap.get(EVALUATION_STATUS)).setRequiredPermission(SecurityPermission.RUN_EVAL_STATUS_REPORT);
 		((ReportType) codeMap.get(ACTION_REPORT)).setRequiredPermission(SecurityPermission.RUN_ACTION_REPORT);
-		((ReportType) codeMap.get(ENTRY_LISTING)).setRequiredPermission(SecurityPermission.RUN_ENTRY_LISTING_REPORT);
-		((ReportType) codeMap.get(ENTRY_STATUS)).setRequiredPermission(SecurityPermission.RUN_ENTRY_STATUS_REPORT);
+		((ReportType) codeMap.get(ENTRIES_BY_CAT_REPORT)).setRequiredPermission(SecurityPermission.RUN_ENTRIES_BY_CAT_REPORT);
+		((ReportType) codeMap.get(ENTRIES_BY_ORG_REPORT)).setRequiredPermission(SecurityPermission.RUN_ENTRIES_BY_ORG_REPORT);
+		((ReportType) codeMap.get(ENTRY_DETAIL_REPORT)).setRequiredPermission(SecurityPermission.RUN_ENTRY_DETAIL_REPORT);
+		((ReportType) codeMap.get(ENTRY_LISTING_REPORT)).setRequiredPermission(SecurityPermission.RUN_ENTRY_LISTING_REPORT);
+		((ReportType) codeMap.get(ENTRY_REPORT)).setRequiredPermission(SecurityPermission.RUN_ENTRY_REPORT);
+		((ReportType) codeMap.get(ENTRY_STATUS_REPORT)).setRequiredPermission(SecurityPermission.RUN_ENTRY_STATUS_REPORT);	
+		((ReportType) codeMap.get(EVAL_STATUS_REPORT)).setRequiredPermission(SecurityPermission.RUN_EVAL_STATUS_REPORT);
+		((ReportType) codeMap.get(LINK_VALIDATION_REPORT)).setRequiredPermission(SecurityPermission.RUN_LINK_VALIDATION_REPORT);
+		((ReportType) codeMap.get(SUBMISSIONS_REPORT)).setRequiredPermission(SecurityPermission.RUN_SUBMISSIONS_REPORT);
+		((ReportType) codeMap.get(USAGE_REPORT)).setRequiredPermission(SecurityPermission.RUN_USAGE_REPORT);
+		((ReportType) codeMap.get(USER_ORG_REPORT)).setRequiredPermission(SecurityPermission.RUN_USER_ORG_REPORT);
+		((ReportType) codeMap.get(USER_REPORT)).setRequiredPermission(SecurityPermission.RUN_USER_REPORT);
 		((ReportType) codeMap.get(WORKPLAN_STATUS)).setRequiredPermission(SecurityPermission.RUN_WORKPLAN_STATUS);
 
 		//update metadata for component type reports
-		((ReportType) codeMap.get(COMPONENT_ORGANIZATION)).setComponentReport(true);
-		((ReportType) codeMap.get(CATEGORY_COMPONENT)).setComponentReport(true);
-		((ReportType) codeMap.get(COMPONENT)).setComponentReport(true);
-		((ReportType) codeMap.get(COMPONENT_DETAIL)).setComponentReport(true);
-		((ReportType) codeMap.get(ENTRY_LISTING)).setComponentReport(true);
-		((ReportType) codeMap.get(ENTRY_STATUS)).setComponentReport(true);
+		((ReportType) codeMap.get(ENTRIES_BY_ORG_REPORT)).setComponentReport(true);
+		((ReportType) codeMap.get(ENTRIES_BY_CAT_REPORT)).setComponentReport(true);
+		((ReportType) codeMap.get(ENTRY_REPORT)).setComponentReport(true);
+		((ReportType) codeMap.get(ENTRY_DETAIL_REPORT)).setComponentReport(true);
+		((ReportType) codeMap.get(ENTRY_LISTING_REPORT)).setComponentReport(true);
+		((ReportType) codeMap.get(ENTRY_STATUS_REPORT)).setComponentReport(true);
 
 		return codeMap;
 	}
