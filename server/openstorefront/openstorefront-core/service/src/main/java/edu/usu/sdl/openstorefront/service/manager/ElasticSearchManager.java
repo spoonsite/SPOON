2f4fa101e948aa2f1615235c44a0d7277801ad39
Gavin Fowler <gavin.fowler@sdl.usu.edu>
2019-03-07 08:09:10 -0700

    SPOON-26 Start the migration of the searchOptions API from services to resources as well as fix the elasticsearch query

--- server/openstorefront/openstorefront-core/service/src/main/java/edu/usu/sdl/openstorefront/service/manager/ElasticSearchManager.java
+++ server/openstorefront/openstorefront-core/service/src/main/java/edu/usu/sdl/openstorefront/service/manager/ElasticSearchManager.java
@@ -410,11 +410,6 @@ public class ElasticSearchManager
 				esQuery.should(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_ORGANIZATION, allLowerQuery));
 				esQuery.should(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_ORGANIZATION, properCaseQuery));
 				esQuery.should(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_ORGANIZATION, actualQuery));
-			} else {
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_ORGANIZATION, allUpperQuery));
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_ORGANIZATION, allLowerQuery));
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_ORGANIZATION, properCaseQuery));
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_ORGANIZATION, actualQuery));
 			}
 
 			if (searchOptions.getCanUseNameInSearch()) {
@@ -428,23 +423,11 @@ public class ElasticSearchManager
 				esQuery.should(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, allLowerQuery));
 				esQuery.should(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, properCaseQuery));
 				esQuery.should(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, actualQuery));
-			} else {
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_NAME, allUpperQuery));
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_NAME, allLowerQuery));
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_NAME, properCaseQuery));
-				esQuery.mustNot(QueryBuilders.wildcardQuery(ComponentSearchView.FIELD_NAME, actualQuery));
-
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, allUpperQuery));
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, allLowerQuery));
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, properCaseQuery));
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, actualQuery));
 			}
 
 			if (searchOptions.getCanUseDescriptionInSearch()) {
 				// Custom query for description
 				esQuery.should(QueryBuilders.matchPhraseQuery("description", actualQuery));
-			} else {
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery("description", actualQuery));	
 			}
 		}
 
@@ -453,22 +436,15 @@ public class ElasticSearchManager
 
 			if (searchOptions.getCanUseOrganizationsInSearch()) {
 				esQuery.should(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_ORGANIZATION, phrase));
-			} else {
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_ORGANIZATION, phrase));
 			}
 
 			if (searchOptions.getCanUseNameInSearch()) {
 				esQuery.should(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, phrase));
-			} else {
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery(ComponentSearchView.FIELD_NAME, phrase));
 			}
 
 			if (searchOptions.getCanUseDescriptionInSearch()) {
 				esQuery.should(QueryBuilders.matchPhraseQuery("description", phrase.toLowerCase()));
-			} else {
-				esQuery.mustNot(QueryBuilders.matchPhraseQuery("description", phrase.toLowerCase()));
 			}
-
 		}
 		FieldSortBuilder sort = new FieldSortBuilder(filter.getSortField())
 				//.unmappedType("String") // currently the only fileds we are searching/sorting on are strings
