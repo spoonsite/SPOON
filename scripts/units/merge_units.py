a621d9f816db127d502a54adae2ddc5350aebb81
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2019-04-23 15:23:09 -0600

    SPOON-982 Check for previously skipped attributes

--- scripts/units/merge_units.py
+++ scripts/units/merge_units.py
@@ -29,7 +29,7 @@ HEADERS = {
 BASE_URL = 'http://localhost:8080/openstorefront'
 ENDPOINT = '/api/v1/resource/attributes/attributetypes'
 
-SKIPPED_FILENAME = 'skipped.txt'
+SKIPPED_ATTR_FILENAME = 'skipped_attributes.txt'
 UNIT_LIST_MAP = {}
 ATTRIBUTE_MAP = {}
 
@@ -51,6 +51,7 @@ def checkUnitPassFail(unit, unit_list):
 def askToMerge(dups, attributes):
     print(f'Found {len(dups)} duplicates')
     val = ''
+    # typeCodes: key == Description, value == List TYPE_CODE
     typeCodes = {}
     units = {}
     listOfCompatible=[]
@@ -79,7 +80,6 @@ def askToMerge(dups, attributes):
     
     # This checks to see if there are any old values that can be used
     # in the new attribute.
-    
     for i in attributes:
         try:
             val = i['attributeValueType']
@@ -118,9 +118,9 @@ def askToMerge(dups, attributes):
 
 
 
-    for i in typeCodes:
-        if len(typeCodes[i]) > 1:
-            
+    not_skipped = filter(lambda x: x not in SKIPPED_ATTR, typeCodes.keys())
+    for desc in not_skipped:
+        if len(typeCodes[desc]) > 1: # && checkUnitPassFail(units[desc], [all other base units])
             current = {
                 'type': '',
                 'detailedDescription':'',
@@ -130,19 +130,22 @@ def askToMerge(dups, attributes):
                 "requiredRestrictions": []
             }
 
-            if i in defaults:
-                current = defaults[i]
+            if desc in defaults:
+                current = defaults[desc]
             else: 
                 print('could not find default')
 
             goodInput = False
 
-            answer = input(f"Do you want to merge the attributes with the name {i}? (Y/N) ")
+            # TODO: check that all attribute attempting to be merged have compatible units
+            #       see /unitchecklist endpoint
+            #       POST { baseUnit: 'g', units: ['g','mg','kg']}
+            answer = input(f"Do you want to merge the attributes with the name {desc}? (Y/N) ")
             while not goodInput:
                 if answer == 'y' or answer == 'Y':
-                    description = input(f"Enter a description, leave blank for \'{i}\' ")
+                    description = input(f"Enter a description, leave blank for \'{desc}\' ")
                     if len(description) == 0:
-                        description = i
+                        description = desc
                     attributeType = input(f"Enter an attribute type, leave blank for \'{current['type']}\' ")
                     if len(attributeType) == 0:
                         attributeType = current['type']
@@ -173,14 +176,14 @@ def askToMerge(dups, attributes):
                     # checks if units from attributes with the same description are compatible
                     listOfCompatible=[]
                     deleteList=[]
-                    for j in typeCodes[i]:
-                        if checkUnitPassFail(attributeUnit, [units[j]]):
-                            listOfCompatible.append(j)
+                    for code in typeCodes[desc]:
+                        if checkUnitPassFail(attributeUnit, [units[code]]):
+                            listOfCompatible.append(code)
 
                     deleteList = [x for x in listOfCompatible if not x == attributeType]
                     print(f"Attributes to be deleted: {deleteList}")
 
-                    temp = input('Press enter to continue...')
+                    temp = input('Press enter to continue (or M. Type \'exit\' to leave loop) ...')
                     if temp == 'm' or temp == 'M':
                         deleteList=[]
                         inputVar = ''
@@ -222,7 +225,9 @@ def askToMerge(dups, attributes):
                         with open(MERGED_PICKLE, 'wb') as fout:
                             pickle.dump(MERGED_UNITS, fout)
                         break
-                else:
+                elif answer == 'N' or answer == 'n':
+                    with open(SKIPPED_ATTR_FILENAME, 'a') as fout:
+                        fout.write(desc + '\n')
                     break
     
 
@@ -241,6 +246,7 @@ def usePickledActions():
 def main():
     global MERGED_UNITS
     global MERGED_PICKLE
+    global SKIPPED_ATTR
 
     banner = r"""
                                                        _               
@@ -258,14 +264,24 @@ def main():
     """
     print(banner)
 
+    # Load previously performed actions
     MERGED_PICKLE = 'merged.pickle'
+    MERGED_UNITS =[]
     try:
         with open(MERGED_PICKLE, 'rb') as fin:
             MERGED_UNITS = pickle.load(fin)
         print(f'Loaded {MERGED_PICKLE} file')
     except:
-        MERGED_UNITS =[]
         print(f'No {MERGED_PICKLE} file')
+
+    # Load previously skipped attribute groupings
+    SKIPPED_ATTR = []
+    try:
+        with open(SKIPPED_ATTR_FILENAME, 'r') as fin:
+            for line in fin:
+                SKIPPED_ATTR.append(line.strip())
+    except:
+        print(f'No {SKIPPED_ATTR_FILENAME} file')
     
     usePickledActions()
 
