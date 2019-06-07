06a964ab1607917c5b4aba0d70f14300af12f22b
Gavin Fowler <gavin.fowler@sdl.usu.edu>
2019-06-07 11:21:48 -0600

    Fix typo

--- server/openstorefront/openstorefront-web/src/main/webapp/OSF/customSubmission/SubmissionFormFullControl.js
+++ server/openstorefront/openstorefront-web/src/main/webapp/OSF/customSubmission/SubmissionFormFullControl.js
@@ -229,7 +229,7 @@ Ext.define('OSF.customSubmission.SubmissionFormFullControl', {
 			if (navButton.sectionIndex === currentSectionIndex) {
 				navButton.addCls('submission-section-nav-select');	
 				
-				//auto scroll is problemmatic
+				//auto scroll is problematic
 				//54 - relative offset
 				//var newY = navButton.getY() - (navPanel.getY() + 54);
 				//navPanel.scrollTo(0, newY, true);								
