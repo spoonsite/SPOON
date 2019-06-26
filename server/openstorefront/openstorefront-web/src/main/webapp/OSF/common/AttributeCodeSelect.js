a8c401e69cbf06ddb8d5a4350fa1097f729ddb09
Gavin Fowler <Gavin Fowler>
2019-06-26 18:04:23 +0000

    SPOON-1050 Add comments and split out confusing ternary

--- server/openstorefront/openstorefront-web/src/main/webapp/OSF/common/AttributeCodeSelect.js
+++ server/openstorefront/openstorefront-web/src/main/webapp/OSF/common/AttributeCodeSelect.js
@@ -89,14 +89,23 @@ Ext.define('OSF.common.AttributeCodeSelect', {
 						if (attributePanel.attributeTypeView.attributeValueType === 'NUMBER') {			
 							//check precision; this will enforce max allowed
 							validatorResponse = CoreUtil.validateNumber(value);
-							valid = valid ? validatorResponse.valid : false;
+
+							// If there are any invalid values, the whole field should be invalid
+							if (valid){
+								valid = validatorResponse.valid;
+							} else {
+								valid = false;
+							}
+
 							msg = validatorResponse.msg;
 						}
+						// If the value is not a number and the type is number or the value is just spaces, the value is invalid
 						if(isNaN(value) || value.trim() == ""){
 							valid = false;
 							msg = "Values must be a number."
 						}
 					});
+					// An Extjs validator function returns either true or a message to display as an error to the user. 
 					return valid ? valid : msg;	
 				};
 			} else {
@@ -105,13 +114,14 @@ Ext.define('OSF.common.AttributeCodeSelect', {
 					var valid = true;
 					var msg = '';
 
+					// Check for values that are only spaces
 					Ext.Array.each(values, function(value) {
 						if(value.trim() == ""){
 							valid = false;
 							msg = "Values cannot be only spaces."
 						}
 					});
-					
+					// An Extjs validator function returns either true or a message to display as an error to the user. 
 					return valid ? valid : msg;	
 				}
 			}
