d15d09f549714df6ae5ce59daae5f15bff45b2a7
Trent Savage <trent.savage@sdl.usu.edu>
2019-09-05 14:29:16 -0600

    SPOON-1127 Remove commented out code

--- server/openstorefront/openstorefront-core/model/src/main/java/edu/usu/sdl/openstorefront/core/view/AttributeTypeAdminView.java
+++ server/openstorefront/openstorefront-core/model/src/main/java/edu/usu/sdl/openstorefront/core/view/AttributeTypeAdminView.java
@@ -48,15 +48,11 @@ public class AttributeTypeAdminView
 		}
 		Service service = ServiceProxyFactory.getServiceProxy();
 
-		// Unnessisary condition check revoked, kept this for future circumstances
-		// if (adminView.getRequiredRestrictions() != null
-		// 		&& !adminView.getRequiredRestrictions().isEmpty()) {
-
-			List<AttributeCode> codes = service.getAttributeService().findCodesForType(attributeType.getAttributeType());
-			if (codes.isEmpty() && !Convert.toBoolean(attributeType.getAllowUserGeneratedCodes())) {
-				adminView.setConfigurationWarning(true);
-			}
-		// }
+		// Check for attribute that are set to disallow user-created codes, yet there are no codes for them to choose.
+		List<AttributeCode> codes = service.getAttributeService().findCodesForType(attributeType.getAttributeType());
+		if (codes.isEmpty() && !Convert.toBoolean(attributeType.getAllowUserGeneratedCodes())) {
+			adminView.setConfigurationWarning(true);
+		}
 
 		return adminView;
 	}
