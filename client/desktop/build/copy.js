5912300338cdc795c0c370d88f95bf52edd7a6da
Trent Savage <trent.savage@sdl.usu.edu>
2019-09-26 09:46:58 -0600

    Modify SEVERAL files to ensure maven lifecycle executes correctly. Several files are git .ignored, so this commit explicitly adds many of them. Explicitly added everything in the client/desktop/ config and build folders, 2 pom.xml files, and package + package-lock.json.

---
+++ client/desktop/build/copy.js
@@ -0,0 +1,9 @@
+const fs = require('fs-extra');
+
+const from = './dist';
+const to = '../../server/openstorefront/openstorefront-web/src/main/webapp/desktop';
+
+// Async with promises:
+fs.copy(from, to)
+  .then(() => console.log(`successfully copied ${from} to ${to}`))
+  .catch(err => console.error(err));
