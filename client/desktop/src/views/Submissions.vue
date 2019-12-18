299fb41da10b1217216c0e0c7a81a4dd3fa4fa38
Katelynn Call <katelynn.call@sdl.usu.edu>
2019-12-18 09:39:49 -0700

    Add the api calls to get the workflows

--- client/desktop/src/views/Submissions.vue
+++ client/desktop/src/views/Submissions.vue
@@ -1,9 +1,24 @@
 <template lang="html">
-  <div>
+  <v-layout
+    row
+    justify-center
+    align-center
+    v-if="isLoading"
+    >
+    <v-flex xs1>
+      <v-progress-circular
+        color="primary"
+        :size="60"
+        :width="6"
+        indeterminate
+      ></v-progress-circular>
+    </v-flex>
+  </v-layout>
+  <div v-else>
     <v-form style="padding: 1em; padding-top: 2em;">
       <div>
         <v-btn class="top-buttons" @click="getUserParts()">Refresh</v-btn>
-        <v-btn class="top-buttons"><v-icon class="fa-xs">fas fa-plus</v-icon>&nbsp; Add New</v-btn>
+        <v-btn class="top-buttons" @click="showData()"><v-icon class="fa-xs">fas fa-plus</v-icon>&nbsp; Add New</v-btn>
         <v-btn class="top-buttons" @click="openBulkUpload()"><v-icon>fas fa-upload</v-icon>&nbsp; Bulk Upload</v-btn>
       </div>
       <div style="display: flex; ">
@@ -32,8 +47,14 @@
               <v-btn small fab class="table-buttons red lighten-3"><v-icon>fas fa-trash</v-icon></v-btn>
             </td>
             <td>
-              <svg width="300" height="50">
-                <circle cx="20" cy="25" r="20" fill="blue" />
+              <svg width="200" height="50">
+                <circle v-for="(step, i) in props.item.approvalWorkflow.steps" :cx="cx" cy="25" r="15" stroke="black" fill="blue" />
+                <line x1="35" y1="25" x2="55" y2="25" style="stroke:black; stroke-width:2"></line>
+                <!-- <circle cx="70" cy="25" r="15" stroke="black" fill="grey" /> -->
+                <line x1="85" y1="25" x2="105" y2="25" style="stroke:black; stroke-width:2"></line>
+                <!-- <circle cx="120" cy="25" r="15" stroke="black" fill="grey" /> -->
+                <line x1="135" y1="25" x2="155" y2="25" style="stroke:black; stroke-width:2"></line>
+                <!-- <circle cx="170" cy="25" r="15" stroke="black" fill="grey" /> -->
               </svg>
             </td>
           </template>
@@ -63,19 +84,56 @@ export default {
         { text: 'Approval Workflow', value: 'approvalWorkflow' }
       ],
       componentDisplay: [],
-      componentData: []
+      componentData: [],
+      isLoading: true,
+      counter: 0,
+      cx: 20
     }
   },
   methods: {
     getUserParts () {
+      this.counter = 0
       this.$http.get(`/openstorefront/api/v1/resource/componentsubmissions`)
         .then(response => {
+          this.isLoading = true
           this.componentData = []
           this.componentDisplay = []
           this.componentData = response.data
           this.formatData()
+          this.getComponentWorkplan()
         })
-        // .catch(e => this.errors.push(e))
+    },
+    getComponentWorkplan () {
+      for (var component in this.componentDisplay) {
+        if (this.componentData[component].componentId) {
+          this.$http.get(`/openstorefront/api/v1/resource/components/${this.componentData[component].componentId}/worklink`)
+          .then(response => {
+            this.componentDisplay[component][approvalWorkflow] = response.data
+
+          })
+          .finally(() => {
+            this.counter++
+            if (this.counter >= this.componentData.length) {
+              this.isLoading = false
+            } 
+          })
+        }
+        else {
+          this.$http.get(`/openstorefront/api/v1/resource/usersubmissions/${this.componentData[component].userSubmissionId}/worklink`)
+          .then(response => {
+            this.componentDisplay[component][approvalWorkflow] = response.data
+            console.log(this.componentDisplay[component].approvalWorkflow)
+            
+          })
+          .finally(() => {
+            this.counter++
+            if (this.counter >= this.componentData.length) {
+              this.isLoading = false
+            }
+          })
+        }
+      }
+      
     },
     formatData () {
       for (var component in this.componentData) {
@@ -123,10 +181,12 @@ export default {
           lastUpdate: this.componentData[component].updateDts
         })
       }
-      console.log(this.componentData)
     },
     openBulkUpload () {
       window.open('openstorefront/bulkUpload.jsp','uploadWin', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=500, height=440')
+    },
+    showData () {
+      console.log(this.componentDisplay)
     }
   }
 }
