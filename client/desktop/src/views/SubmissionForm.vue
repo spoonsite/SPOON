59a574483f29a94a0cc37dae7207d6085dee717b
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2020-01-13 17:08:34 -0700

    SPOON-1226 Fix attribute styling

--- client/desktop/src/views/SubmissionForm.vue
+++ client/desktop/src/views/SubmissionForm.vue
@@ -6,8 +6,8 @@
       <p>{{ $store.state.branding.userInputWarning }}</p>
     </div>
     <v-form v-model="isFormValid" ref="submissionForm" style="width: 80%;" class="mx-auto">
-      <fieldset>
-        <legend class="title">Entry Details*</legend>
+      <fieldset class="fieldset">
+        <legend class="legend title">Entry Details*</legend>
         <v-text-field
           label="Entry Title*"
           v-model="entryTitle"
@@ -46,15 +46,15 @@
           class="mx-4"
         />
       </fieldset>
-      <fieldset>
-        <legend class="title">Primary Point of Contact*</legend>
+      <fieldset class="fieldset">
+        <legend class="title legend">Primary Point of Contact*</legend>
         <v-text-field label="First Name*" v-model="firstName" required :rules="[rules.required]" class="mx-4" />
         <v-text-field label="Last Name*" v-model="lastName" required :rules="[rules.required]" class="mx-4" />
         <v-text-field label="Email*" v-model="email" required :rules="[rules.required]" class="mx-4" />
         <v-text-field label="Phone*" v-model="phone" required :rules="[rules.required]" class="mx-4" />
       </fieldset>
-      <fieldset>
-        <legend class="title">Image Upload</legend>
+      <fieldset class="fieldset">
+        <legend class="title legend">Image Upload</legend>
         <v-row v-for="(item, index) in images" :key="index" class="mx-4">
           <v-col cols="12" md="2">
             <v-img :src="item.img" alt="No preview available" max-height="90px" max-width="120px" contain />
@@ -66,6 +66,7 @@
               required
               :rules="[rules.required]"
               @change="imageChange(index)"
+              accept=".png,.svg,.jpeg,.jpg"
             />
           </v-col>
           <v-col cols="12" md="5">
@@ -77,95 +78,124 @@
         </v-row>
         <v-btn color="grey lighten-2" class="ma-4" @click="addImage"><v-icon left>mdi-plus</v-icon>Add image</v-btn>
       </fieldset>
-      <fieldset style="height: 500px">
-        <legend class="title">Description*</legend>
-        <quill-editor style="height:400px;" class="ma-2" v-model="description" />
+      <fieldset class="fieldset">
+        <legend class="title legend">Description*</legend>
+        <quill-editor class="ma-2" v-model="description" />
       </fieldset>
-      <fieldset>
-        <legend class="title">Attributes</legend>
+      <fieldset class="fieldset">
+        <legend class="title legend">Attributes</legend>
         <div class="mx-4 mt-4">
           <div v-if="attributes.required.length === 0">
             No required attributes available, please select an entry type
           </div>
           <div v-else>
-            <h2>Required Attributes:</h2>
-            <div v-for="attribute in attributes.required" :key="attribute.attributeType" class="mx-2">
-              <p style="word-wrap: wrap" class="pt-5 pr-4">{{ attribute.description }}</p>
-              <v-text-field
-                v-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'TEXT'"
-                v-model="attribute.selectedCodes"
-                label="Value"
-              />
-              <v-text-field
-                v-else-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'NUMBER'"
-                v-model="attribute.selectedCodes"
-                label="Value"
-                :rules="[rules.numberOnly]"
-              />
-              <v-select v-else label="Value" clearable :items="attribute.codes" item-text="label" item-code="code" />
-              <v-select
-                label="Unit"
-                v-if="attribute.attributeValueType === 'NUMBER' && attribute.attributeUnit !== ''"
-                :value="attribute.attributeUnit"
-                :items="attribute.attributeUnitList"
-                item-text="unit"
-                item-value="unit"
-              />
-            </div>
-            <br />
+            <fieldset class="fieldset">
+              <legend class="title legend">Required Attributes</legend>
+              <div class="attribute-row" v-for="attribute in attributes.required" :key="attribute.attributeType">
+                <label :for="attribute.description" class="mr-3">{{ attribute.description }}</label>
+                <v-text-field
+                  v-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'TEXT'"
+                  v-model="attribute.selectedCodes"
+                  label="Value"
+                  class="mr-3"
+                  :id="attribute.description"
+                />
+                <v-text-field
+                  v-else-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'NUMBER'"
+                  v-model="attribute.selectedCodes"
+                  label="Value"
+                  :rules="[rules.numberOnly]"
+                  class="mr-3"
+                  :id="attribute.description"
+                />
+                <v-select
+                  v-else
+                  label="Value"
+                  clearable
+                  :items="attribute.codes"
+                  item-text="label"
+                  item-code="code"
+                  :id="attribute.description"
+                />
+                <v-select
+                  label="Unit"
+                  v-if="attribute.attributeValueType === 'NUMBER' && attribute.attributeUnit !== ''"
+                  :value="attribute.attributeUnit"
+                  :items="attribute.attributeUnitList"
+                  item-text="unit"
+                  item-value="unit"
+                  class="mr-3"
+                />
+              </div>
+            </fieldset>
           </div>
           <div v-if="attributes.suggested.length === 0">
             No required attributes available, please select an entry type
           </div>
           <div v-else>
-            <h2>Suggested Attributes:</h2>
-            <div v-for="attribute in attributes.suggested" :key="attribute.attributeType" class="mx-2">
-              <p style="word-wrap: wrap" class="pt-5 pr-4">{{ attribute.description }}</p>
-              <v-text-field
-                v-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'TEXT'"
-                v-model="attribute.selectedCodes"
-                label="Value"
-              />
-              <v-text-field
-                v-else-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'NUMBER'"
-                v-model="attribute.selectedCodes"
-                label="Value"
-                :rules="[rules.numberOnly]"
-              />
-              <v-select v-else label="Value" clearable :items="attribute.codes" item-text="label" item-code="code" />
-              <v-select
-                label="Unit"
-                v-if="attribute.attributeValueType === 'NUMBER' && attribute.attributeUnit !== ''"
-                :value="attribute.attributeUnit"
-                :items="attribute.attributeUnitList"
-                item-text="unit"
-                item-value="unit"
-              />
-            </div>
-            <br />
+            <fieldset class="fieldset">
+              <legend class="title legend">Suggested Attributes</legend>
+              <div class="attribute-row" v-for="attribute in attributes.suggested" :key="attribute.attributeType">
+                <label :for="attribute.description" class="mr-3">{{ attribute.description }}</label>
+                <v-text-field
+                  v-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'TEXT'"
+                  v-model="attribute.selectedCodes"
+                  label="Value"
+                  class="mr-3"
+                  :id="attribute.description"
+                />
+                <v-text-field
+                  v-else-if="attribute.allowUserGeneratedCodes && attribute.attributeValueType === 'NUMBER'"
+                  v-model="attribute.selectedCodes"
+                  label="Value"
+                  :rules="[rules.numberOnly]"
+                  class="mr-3"
+                  :id="attribute.description"
+                />
+                <v-select
+                  v-else
+                  label="Value"
+                  clearable
+                  :items="attribute.codes"
+                  item-text="label"
+                  item-code="code"
+                  :id="attribute.description"
+                />
+                <v-select
+                  label="Unit"
+                  v-if="attribute.attributeValueType === 'NUMBER' && attribute.attributeUnit !== ''"
+                  :value="attribute.attributeUnit"
+                  :items="attribute.attributeUnitList"
+                  item-text="unit"
+                  item-value="unit"
+                  class="mr-3"
+                />
+              </div>
+            </fieldset>
           </div>
         </div>
         <div class="mx-4 mt-4">
           <strong>Request New Attribute</strong>
           <p class="mb-0">Please describe the attribute you would like to have added.</p>
-          <p class="mb-0">
+          <p class="mb-3">
             Include the value for your entry, a brief description, and how your part is defined by the attribute.
           </p>
-          <v-textarea label="Request New Attribute (opt.)" v-model="attributes.missingAttribute" />
+          <label class="title" for="request-new-attribute">Request New Attribute (opt.)</label>
+          <quill-editor id="request-new-attribute" class="ma-2" v-model="attributes.missingAttribute" />
         </div>
       </fieldset>
-      <fieldset>
-        <legend class="title">Resources</legend>
+      <fieldset class="fieldset">
+        <legend class="title legend">Resources</legend>
         <h3>Resources here</h3>
         <v-text-field label="Media Description" />
         <v-file-input label="Add file" />
       </fieldset>
-      <fieldset>
-        <legend class="title">Tags</legend>
+      <fieldset class="fieldset">
+        <legend class="title legend">Tags</legend>
         <h3>Tags here</h3>
       </fieldset>
-      <fieldset>
-        <legend class="title">Contacts</legend>
+      <fieldset class="fieldset">
+        <legend class="title legend">Contacts</legend>
         <h3>Contacts here here</h3>
       </fieldset>
       <v-btn color="primary" @click="submit">
@@ -256,6 +286,7 @@ export default {
     },
     imageChange(index) {
       let e = this.images[index]
+      // test that the file is an image
       let reader = new FileReader()
       reader.onloadend = function() {
         e.img = reader.result
@@ -277,15 +308,23 @@ export default {
 }
 </script>
 
-<style>
-fieldset {
+<style lang="css" scoped>
+.fieldset {
   border: 1px solid rgba(0,0,0,0.2);
   background-color: white;
   border-radius: 10px;
   margin: 2em 0;
-  padding: 6px;
+  padding: 1em;
+  padding-bottom: 12px;
 }
-legend {
+.legend {
   margin-left: 1em;
+}
+.attribute-row {
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: 1fr 3fr 1fr;
+  align-items: center;
+  padding: 0 1em;
 }
 </style>
