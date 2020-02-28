f4bfa5ae2aee270640fbcb58f2cd52d6a2bd3330
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2020-02-28 10:41:37 -0700

    Fix user created tags

--- client/desktop/src/views/SubmissionForm.vue
+++ client/desktop/src/views/SubmissionForm.vue
@@ -872,7 +872,7 @@ export default {
               attributeCode: this.convertNumber(el.selectedCodes, conversionFactor)
             }
           })
-        } else if (typeof el.selectedCodes === 'object' && el.selectedCodes) {
+        } else if (typeof el.selectedCodes === 'object' && el.selectedCodes && !Array.isArray(el.selectedCodes)) {
           newAttributes.push({
             componentAttributePk: {
               userCreated: el.userCreated,
@@ -1265,7 +1265,24 @@ export default {
     addTag() {
       this.tagsList.push({ text: this.tagSearchText })
       this.tags.push({ text: this.tagSearchText })
+      this.addNewTag(this.tagSearchText)
       this.tagSearchText = ''
+      // request new tag to be added
+    },
+    addNewTag(text) {
+      this.$http
+        .post(`/openstorefront/api/v1/resource/components/${this.id}/tags`, {
+          text: text
+        })
+        .then(res => {
+          // fetch all tags for submission
+          // inefficient but it guarantees that the tagIds are valid
+          this.getTags()
+        })
+        .catch(e => {
+          this.$toasted.error('There was a problem adding a tag to the submission')
+          console.error('Problem adding tag: ', text)
+        })
     },
     addContact() {
       this.contacts.push({ firstName: '', lastName: '', contactType: '', organization: '', email: '', phone: '' })
@@ -1319,19 +1336,7 @@ export default {
       let removedTag = _.differenceBy(oldVal, newVal, 'text')
       if (newTag && newTag.length > 0) {
         // add new tag
-        this.$http
-          .post(`/openstorefront/api/v1/resource/components/${this.id}/tags`, {
-            text: newTag[0].text
-          })
-          .then(res => {
-            // fetch all tags for submission
-            // inefficient but it guarantees that the tagIds are valid
-            this.getTags()
-          })
-          .catch(e => {
-            this.$toasted.error('There was a problem adding a tag to the submission')
-            console.error('Problem adding tag: ', newTag)
-          })
+        this.addNewTag(newTag[0].text)
       }
       if (removedTag && removedTag.length > 0) {
         // add new tag
