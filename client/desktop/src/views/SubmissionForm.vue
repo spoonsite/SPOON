2daf5836c5a8fc447b7130f1e78f590ce9f84acf
Katelynn Call <katelynn.call@sdl.usu.edu>
2020-03-03 17:20:28 +0000

    client/desktop/src/views/SubmissionForm.vue edited online with Bitbucket

--- client/desktop/src/views/SubmissionForm.vue
+++ client/desktop/src/views/SubmissionForm.vue
@@ -643,7 +643,7 @@ export default {
     }
     this.$http.get('/openstorefront/api/v1/resource/organizations').then(response => {
       this.organizationList = response.data.data
-      this.organizationList.sort((a, b) => (a.name > b.name ? 1 : -1))
+      this.organizationList.sort((a, b) => a.name > b.name)
     })
     this.$http.get('/openstorefront/api/v1/resource/lookuptypes/SecurityMarkingType').then(response => {
       this.securityMarkingList = response.data
