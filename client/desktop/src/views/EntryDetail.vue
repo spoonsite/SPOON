85699f10cb2db814b7191ccbe032db8661c8abb8
Gavin Fowler <gavin.fowler@sdl.usu.edu>
2020-03-04 15:18:13 +0000

    SPOON-1411 Add checking on dates

--- client/desktop/src/views/EntryDetail.vue
+++ client/desktop/src/views/EntryDetail.vue
@@ -36,10 +36,12 @@
               <p v-if="detail.lastSubmitDts" class="pb-0 mb-1">
                 <strong>Last Vendor Update Provided:</strong> {{ detail.lastSubmitDts | formatDate }}
               </p>
-              <p v-else class="pb-1 mb-1">
+              <p v-else-if="detail.approvedDate" class="pb-1 mb-1">
                 <strong>Last Vendor Update Provided:</strong> {{ detail.approvedDate | formatDate }}
               </p>
-              <p class="pb-1 mb-1"><strong>Last System Update:</strong> {{ detail.lastActivityDts | formatDate }}</p>
+              <p v-if="detail.lastActivityDts" class="pb-1 mb-1">
+                <strong>Last System Update:</strong> {{ detail.lastActivityDts | formatDate }}
+              </p>
             </div>
             <div style="padding-bottom: 1em;" class="clearfix tags" v-if="detail.tags && detail.tags.length !== 0">
               <span v-for="tag in detail.tags" :key="tag.text" style="margin-right: 0.8em;;">
