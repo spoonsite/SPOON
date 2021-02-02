a87694f48273e4c9a1e8a0fa723b2acd85be691b
Ryan Frazier <ryan.frazier@sdl.usu.edu>
2021-02-01 17:17:44 -0700

    Enable delete of component resources

--- server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/resource/ComponentSubmissionResource.java
+++ server/openstorefront/openstorefront-web/src/main/java/edu/usu/sdl/openstorefront/web/rest/resource/ComponentSubmissionResource.java
@@ -607,13 +607,7 @@ public class ComponentSubmissionResource
 							componentAll.getMedia().clear();
 							componentAll.getMedia().addAll(mediaMap.values());
 
-							ComponentResource componentResourceExample = new ComponentResource();
-							componentResourceExample.setActiveStatus(ComponentResource.ACTIVE_STATUS);
-							componentResourceExample.setComponentId(exstingComponent.getComponentId());
-							List<ComponentResource> componentResources = service.getPersistenceService().queryByExample(componentResourceExample);
-							componentAll.getResources().addAll(componentResources);
-
-							//clean out duplicate resouces
+							//clean out duplicate resources
 							Map<String, ComponentResource> resourceMap = new HashMap<>();
 							for (ComponentResource resource : componentAll.getResources()) {
 								String id = resource.getResourceId();
